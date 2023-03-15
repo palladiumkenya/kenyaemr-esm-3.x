be444a75900b104daf71110e46c6b748491ba9ac
CynthiaKamau <cynthiakamau54@gmail.com>
2023-03-15 15:45:17 +0300

    (feat) Updated patient chart link and loaded results from patient id endpoint (#11)

--- packages/esm-patient-flags-app/src/hooks/usePatientId.tsx
+++ packages/esm-patient-flags-app/src/hooks/usePatientId.tsx
@@ -2,11 +2,11 @@ import { openmrsFetch } from '@openmrs/esm-framework';
 import useSWR from 'swr';
 
 const usePatientId = (patientUuid: string) => {
-  const { isLoading, data, error } = useSWR<{ data: { patientId: string; age: number } }>(
+  const { isLoading, data, error } = useSWR<{ data: { results: { patientId: string; age: number }} }>(
     `/ws/rest/v1/kenyaemr/patient?patientUuid=${patientUuid}`,
     openmrsFetch,
   );
-  return { patient: data?.data, error, isLoading };
+  return { patient: data?.data?.results, error, isLoading };
 };
 
 export default usePatientId;
