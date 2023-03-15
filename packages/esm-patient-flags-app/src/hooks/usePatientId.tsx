2a93f44f4dc5485dfab0a7f545ed42ba875291ac
CynthiaKamau <cynthiakamau54@gmail.com>
2023-03-15 16:05:40 +0300

    (fix) Fixed build error (#12)

--- packages/esm-patient-flags-app/src/hooks/usePatientId.tsx
+++ packages/esm-patient-flags-app/src/hooks/usePatientId.tsx
@@ -2,7 +2,7 @@ import { openmrsFetch } from '@openmrs/esm-framework';
 import useSWR from 'swr';
 
 const usePatientId = (patientUuid: string) => {
-  const { isLoading, data, error } = useSWR<{ data: { results: { patientId: string; age: number }} }>(
+  const { isLoading, data, error } = useSWR<{ data: { results: { patientId: string; age: number } } }>(
     `/ws/rest/v1/kenyaemr/patient?patientUuid=${patientUuid}`,
     openmrsFetch,
   );
