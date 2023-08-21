7260291e88086eea4723c01f321787a4123c37f5
Anjula Shanaka <anjulashanaka@gmail.com>
2023-08-21 19:18:52 +0530

    Setup E2E test environment with playwright (#30)
    * Setup E2E test environment with playwright
    
    * Add env file to gitignore
    
    * Update the github action

---
+++ e2e/specs/sample-test.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '../core';
+import { expect } from '@playwright/test';
+import { HomePage } from '../pages';
+
+test('Go to homepage', async ({ page }) => {
+  const homePage = new HomePage(page);
+
+  await test.step('When I visit the home page', async () => {
+    await homePage.gotoHome();
+  });
+
+  await test.step('Then should be at the home page', async () => {
+    await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/home`);
+  });
+});
