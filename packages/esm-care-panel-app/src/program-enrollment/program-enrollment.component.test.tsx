99f3b28655eef036ca6551eee871cfbeb7f0a24e
Donald Kibet <chelashawdonald@yahoo.com>
2023-09-28 07:35:30 +0300

    (fix) Fix: Correctly launch enrollment forms in edit or discontinuation mode (#39)

--- packages/esm-care-panel-app/src/program-enrollment/program-enrollment.component.test.tsx
+++ packages/esm-care-panel-app/src/program-enrollment/program-enrollment.component.test.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ProgramEnrollment, { ProgramEnrollmentProps } from './program-enrollment.component';
+import { showNotification } from '@openmrs/esm-framework';
+
+const mockLaunchWorkspace = jest.fn();
+const mockShowNotification = jest.fn();
+
+jest.mock('@openmrs/esm-framework', () => ({
+  ...jest.requireActual('@openmrs/esm-framework'),
+  showNotification: jest.fn(),
+}));
 
 describe('ProgramEnrollment Component', () => {
   const mockProps: ProgramEnrollmentProps = {
@@ -21,7 +30,7 @@ describe('ProgramEnrollment Component', () => {
       },
     ],
     formEntrySub: jest.fn(),
-    launchPatientWorkspace: jest.fn(),
+    launchPatientWorkspace: mockLaunchWorkspace,
   };
 
   it('displays active program enrollment details correctly', () => {
@@ -68,5 +77,55 @@ describe('ProgramEnrollment Component', () => {
     expect(screen.getByText(/Stage 2/i)).toBeInTheDocument();
     expect(screen.getByText(/Entry Point/i)).toBeInTheDocument();
     expect(screen.getByText(/IPD/i)).toBeInTheDocument();
+  });
+
+  test('should show notification error when there is no encounter when launching forms', () => {
+    render(<ProgramEnrollment {...mockProps} />);
+
+    const editButton = screen.getByText(/Edit/i);
+    const deleteButton = screen.getByText(/Discontinue/i);
+
+    expect(editButton).toBeInTheDocument();
+    expect(deleteButton).toBeInTheDocument();
+
+    fireEvent.click(editButton);
+    expect(showNotification).toHaveBeenCalledWith({
+      title: 'Edit enrollment',
+      kind: 'error',
+      description:
+        'The enrollment form does not have an encounter associated with it, Please contact your system administrator to add an encounter to the enrollment',
+    });
+  });
+
+  test("should launch patient workspace when clicking on 'Edit' button", () => {
+    const updatedMockProps = {
+      ...mockProps,
+      data: [
+        {
+          ...mockProps.data[0],
+          data: [
+            {
+              ...mockProps.data[0].data[0],
+              enrollmentFormUuid: 'encounter-123',
+              enrollmentFormName: 'some form name',
+              enrollmentEncounterUuid: 'some-encounter-uuid',
+              discontinuationFormUuid: 'discontinuation-123',
+              discontinuationFormName: 'some form name',
+            },
+          ],
+        },
+      ],
+    };
+    render(<ProgramEnrollment {...updatedMockProps} />);
+
+    // Trigger the edit button
+    const editButton = screen.getByText(/Edit/i);
+
+    fireEvent.click(editButton);
+    expect(mockLaunchWorkspace).toHaveBeenCalled();
+    expect(mockLaunchWorkspace).toHaveBeenCalledWith('patient-form-entry-workspace', {
+      formInfo: { encounterUuid: 'some-encounter-uuid', formUuid: 'encounter-123' },
+      workspaceTitle: 'some form name',
+    });
   });
 });
