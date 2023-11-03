39f7d8fbd465679c03ff3e68b2cfbb8f5ec2e9e3
Donald Kibet <chelashawdonald@yahoo.com>
2023-11-03 10:58:41 +0300

    (upgrade yarn to 4.0.1 to fix build

--- packages/esm-version-app/src/release-version.js
+++ packages/esm-version-app/src/release-version.js
@@ -2,15 +2,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "v5.0.0.0-24-g4a5d634-dirty",
-    "hash": "g4a5d634",
-    "distance": 24,
+    "dirty": false,
+    "raw": "v5.0.0.0-25-gbf8152b",
+    "hash": "gbf8152b",
+    "distance": 25,
     "tag": "v5.0.0.0",
     "semver": null,
-    "suffix": "24-g4a5d634-dirty",
+    "suffix": "25-gbf8152b",
     "semverString": "",
     "version": "5.1.0",
-    "buildDate": "2023-11-03T07:30:28.475Z"
+    "buildDate": "2023-11-03T07:35:13.288Z"
 };
 /* tslint:enable */
