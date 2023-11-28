dc1c83e8f28aa079ff9a95ddcfb8b0dfe59d3b1e
Dennis Kigen <kigen.work@gmail.com>
2023-11-28 18:45:25 +0300

    (fix) Tweak billing header UI (#67)
    * (fix) Tweak billing header UI
    
    * Fixup

--- packages/esm-billing-app/src/root.component.tsx
+++ packages/esm-billing-app/src/root.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { BillingDashboard } from './billing-dashboard.component';
+import { BillingDashboard } from './billing-dashboard/billing-dashboard.component';
 
 const RootComponent: React.FC = () => {
   const baseName = window.getOpenmrsSpaBase() + 'home/billing';
