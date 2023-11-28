d60ddecc6993acceb1f4b968928e6557770bb3d2
CynthiaKamau <cynthiakamau54@gmail.com>
2023-11-28 14:33:19 +0300

    (feat) Add billing module header

--- packages/esm-billing-app/src/billing-dashboard.component.tsx
+++ packages/esm-billing-app/src/billing-dashboard.component.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { Layer, Tile } from '@carbon/react';
 import { EmptyDataIllustration } from '@openmrs/esm-patient-common-lib';
 import styles from './billing-dashboard.scss';
+import BillingHeader from './billing-header/billing-header.component';
+import { useTranslation } from 'react-i18next';
 
 export function BillingDashboard() {
+  const { t } = useTranslation();
+
   return (
-    <Layer className={styles.emptyStateContainer}>
-      <Tile className={styles.tile}>
-        <div className={styles.illo}>
-          <EmptyDataIllustration />
-        </div>
-        <p className={styles.content}>There are no billing records to display.</p>
-      </Tile>
-    </Layer>
+    <>
+      <BillingHeader title={t('billing', 'Billing')} />
+      <Layer className={styles.emptyStateContainer}>
+        <Tile className={styles.tile}>
+          <div className={styles.illo}>
+            <EmptyDataIllustration />
+          </div>
+          <p className={styles.content}>{t('noBillingRecords', 'There are no billing records to display')}</p>
+        </Tile>
+      </Layer>
+    </>
   );
 }
