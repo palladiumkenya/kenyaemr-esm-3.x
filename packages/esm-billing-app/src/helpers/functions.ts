523b539d352763874324d0d486bc997e2930bf6d
CynthiaKamau <cynthiakamau54@gmail.com>
2023-11-30 10:34:25 +0300

    (feat) Add helper functions to get total bill and balance (#73)

---
+++ packages/esm-billing-app/src/helpers/functions.ts
@@ -0,0 +1,32 @@
+import { Payment, LineItem } from '../types';
+
+// amount already paid
+export function calculateTotalAmountTendered(payments: Array<Payment>) {
+  return Array.isArray(payments)
+    ? payments.reduce((totalAmount, item) => {
+        // Ensure that "amount" property is present and numeric
+        if (typeof item.amount === 'number' && item.voided !== true) {
+          return totalAmount + item.amount;
+        }
+        return totalAmount;
+      }, 0)
+    : 0;
+}
+
+// balance
+export function calculateTotalBalance(lineItems: Array<LineItem>, payments: Array<Payment>) {
+  return Math.min(this.calculateTotalAmount(lineItems) - this.calculateTotalAmountTendered(payments));
+}
+
+// total bill
+export function calculateTotalAmount(lineItems: Array<LineItem>) {
+  return Array.isArray(lineItems)
+    ? lineItems.reduce((totalAmount, item) => {
+        // Ensure that "price" and "quantity" properties are present and numeric
+        if (typeof item.price === 'number' && typeof item.quantity === 'number' && item.voided !== true) {
+          return totalAmount + item.price * item.quantity;
+        }
+        return totalAmount;
+      }, 0)
+    : 0;
+}
