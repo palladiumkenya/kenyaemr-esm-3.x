91d1c40aaa67919fb990735f5724caef655c595c
CynthiaKamau <cynthiakamau54@gmail.com>
2023-12-14 20:58:52 +0300

    code review

--- packages/esm-patient-flags-app/src/config-schema.ts
+++ packages/esm-patient-flags-app/src/config-schema.ts
@@ -1,1 +1,14 @@
-export const configSchema = {};
+import { Type } from '@openmrs/esm-framework';
+
+export const configSchema = {
+  facilityDashboardUrl: {
+    _type: Type.String,
+    _description: 'Facility dashboard link for superset analytics',
+    _default:
+      'http://localhost:8088/superset/dashboard/11/?native_filters_key=qyGypN3sBN9g7IYbvZJc51SXLyEYcONEZ3lpUuILe_22hewewKf4U_jNHRVwg9y2',
+  },
+};
+
+export type ConfigObject = {
+  facilityDashboardUrl: string;
+};
