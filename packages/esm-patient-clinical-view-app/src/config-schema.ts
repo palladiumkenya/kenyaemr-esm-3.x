52f24afb1108942d575740dc968ae41346eca6f3
ojwanganto <ojwangantony.g7kenya@gmail.com>
2023-12-15 22:24:58 +0300

    add views for hts screening and testing

--- packages/esm-patient-clinical-view-app/src/config-schema.ts
+++ packages/esm-patient-clinical-view-app/src/config-schema.ts
@@ -6,6 +6,7 @@ export const configSchema = {
     _description: 'List of encounter type UUIDs',
     _default: {
       mchMotherConsultation: 'c6d09e05-1f25-4164-8860-9f32c5a02df0',
+      hivTestingServices: '9c0a7a57-62ff-4f75-babe-5835b0e921b7',
     },
   },
   formsList: {
@@ -15,11 +16,23 @@ export const configSchema = {
       antenatal: 'e8f98494-af35-4bb8-9fc7-c409c8fed843',
       postNatal: '72aa78e0-ee4b-47c3-9073-26f3b9ecc4a7',
       labourAndDelivery: '496c7cc3-0eea-4e84-a04c-2292949e2f7f',
+      htsScreening: '04295648-7606-11e8-adc0-fa7ae01bbebc',
+      htsInitialTest: '402dc5d7-46da-42d4-b2be-f43ea4ad87b0',
+      htsRetest: 'b08471f6-0892-4bf7-ab2b-bf79797b8ea4',
+      htsLinkage: '050a7f12-5c52-4cad-8834-863695af335d',
+      htsReferral: '9284828e-ce55-11e9-a32f-2a2ae2dbcce4',
     },
   },
 };
 
 export interface ConfigObject {
-  encounterTypes: { mchMotherConsultation: string };
-  formsList: { labourAndDelivery: string; antenatal: string; postnatal: string };
+  encounterTypes: { mchMotherConsultation: string; hivTestingServices: string };
+  formsList: {
+    labourAndDelivery: string;
+    antenatal: string;
+    postnatal: string;
+    htsScreening: string;
+    htsInitialTest: string;
+    htsRetest: string;
+  };
 }
--- packages/esm-patient-clinical-view-app/src/index.ts
+++ packages/esm-patient-clinical-view-app/src/index.ts
@@ -2,7 +2,7 @@ import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmr
 import { configSchema } from './config-schema';
 import { createDashboardLink, registerWorkspace } from '@openmrs/esm-patient-common-lib';
 import MaternalHealthList from './esm-mch-app/views/maternal-health/maternal-health.component';
-
+import HivTestingEncountersList from './esm-hts-app/views/hiv-testing/hiv-testing-services.component';
 const moduleName = '@kenyaemr/esm-patient-clinical-view-app';
 
 const options = {
@@ -11,7 +11,9 @@ const options = {
 };
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
-export const mchClinicalView = getSyncLifecycle(MaternalHealthList, options);
+// export const mchClinicalView = getSyncLifecycle(MaternalHealthList, options);
+export const htsClinicalView = getSyncLifecycle(HivTestingEncountersList, options);
+
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 }
