10916203ccc58b18df258bd845badf04f22306e6
ojwanganto <ojwangantony.g7kenya@gmail.com>
2023-12-20 00:05:56 +0300

    refine extraction and presentation of variables in mch clinical views

---
+++ packages/esm-patient-clinical-view-app/src/esm-mch-app/views/maternal-health/labour-and-delivery-care-constants.ts
@@ -0,0 +1,255 @@
+export const labourAndDeliveryConceptMap = {
+  '162054AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Admission Number:',
+    answers: {},
+  },
+  '1590AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Number of ANC Visits:',
+    answers: {},
+  },
+  '1427AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Date of Last Menstrual Period(LMP)',
+    answers: {},
+  },
+  '299AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'VDRL/RPR Results:',
+    answers: {
+      '703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Positive',
+      '664AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Negative',
+      '1118AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Not Done',
+    },
+  },
+  '5596AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Estimated date of Delivery',
+    answers: {},
+  },
+  '163454AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Placenta complete?',
+    answers: {
+      '703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '664AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1501AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Baby born before arrival',
+    },
+  },
+  '160704AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Vagina Examination?',
+    answers: {
+      '132681AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Normal',
+      '5577AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Episiotomy',
+      '159264AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Vaginal tear',
+      '118935AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'FGM',
+      '139505AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Vaginal warts',
+    },
+  },
+  '1282AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'NVP for the baby dispensed?',
+    answers: {
+      '80586AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1175AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'N/A',
+    },
+  },
+  '1789AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Gestation at birth (in weeks):',
+    answers: {},
+  },
+  '159616AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Duration of Labor (Hours):',
+    answers: {},
+  },
+  '5630AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Mode of Delivery:',
+    answers: {
+      '1170AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Spontaneous vaginal delivery(SVD)',
+      '1171AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Cesarean section(CS)',
+      '1172AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Breech delivery',
+      '118159AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Assisted vaginal delivery(AVD)',
+    },
+  },
+  '5599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Date and time of Delivery:',
+    answers: {},
+  },
+  '161928AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Blood loss during delivery in millilitres (mls):',
+    answers: {},
+  },
+  '1856AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Condition of Mother after delivery:',
+    answers: {
+      '160429AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Alive',
+      '134612AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Dead',
+    },
+  },
+  '162131AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Resuscitation Done:',
+    answers: {
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+    },
+  },
+  '1602AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Maternal death audited?',
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+    },
+  },
+  '120216AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Delivery complications ?',
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1067AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Unknown',
+    },
+  },
+  '1572AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Place of Delivery:',
+    answers: {
+      '1536AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Home',
+      '1589AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Facility',
+      '1601AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Born Before Arrival',
+    },
+  },
+  '1576AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Select delivery complications ?',
+    answers: {
+      '1067AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Unknown',
+      '113195AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Ruptured Uterus',
+      '118744AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Eclampsia',
+      '228AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'APH',
+      '230AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'PPH',
+      '115036AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Obstructed Labor',
+      '130AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Puerperal sepsis',
+    },
+  },
+  '162093AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Other delivery complications',
+    answers: {},
+  },
+  '1473AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Delivery Conducted By:',
+    answers: {},
+  },
+  '1573AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Cadre :',
+    answers: {
+      '1574AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Clinical officer/Doctor',
+      '1578AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Midwife',
+      '1577AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Nurse',
+      '1575AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Traditional Birth Attendant',
+      '1555AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Community Health Care Worker',
+      '5622AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Other',
+    },
+  },
+  '159949AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Delivery Outcome',
+    answers: {
+      '159913AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Single',
+      '159914AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Twins',
+      '159915AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Triplets',
+    },
+  },
+  '159369AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Chlorhexidine applied on cord stump:',
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+    },
+  },
+  '162588AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Baby details',
+    answers: {},
+  },
+  '164410AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'HIV testing',
+    answers: {},
+  },
+  '1396AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'HIV Status:',
+    answers: {
+      '703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Positive',
+      '664AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Negative',
+      '1067AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Unknown',
+      '164817AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Known Positive',
+    },
+  },
+  '161557AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: "Has the client's partner been tested for HIV? Partner HIV Status:",
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+    },
+  },
+  '1436AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Partner HIV Status:',
+    answers: {
+      '703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'HIV Positive',
+      '664AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'HIV Negative',
+      '1067AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Unknown',
+    },
+  },
+  '1109AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: "Mother's prophylaxis given during this visit?",
+    answers: {
+      '105281AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Cotrimoxazole',
+      '74250AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Dapsone',
+      '1107AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'None',
+    },
+  },
+  '166665AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Mother started on HAART at maternity ?',
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1175AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'N/A',
+    },
+  },
+  '163783AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Was mother on HAART during ANC?',
+    answers: {
+      '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Yes',
+      '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'No',
+      '1175AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'N/A',
+    },
+  },
+  '163145AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Referred to?',
+    answers: {
+      '1537AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Another Health Facility',
+      '163488AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Community Unit',
+      '1175AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'N/A',
+    },
+  },
+  '164359AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Reasons for referral? ',
+    answers: {},
+  },
+  '5096AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Next appointment date:',
+    answers: {},
+  },
+  '159395AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Clinical notes',
+    answers: {},
+  },
+  '1586AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: "Baby's name",
+    answers: {},
+  },
+  '1587AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Sex',
+    answers: {
+      '1534AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Male',
+      '1535AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Female',
+    },
+  },
+  '159917AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': {
+    display: 'Condition of the baby',
+    answers: {
+      '151849AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Live birth',
+      '159916AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Fresh still birth',
+      '135436AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA': 'Macerated still birth',
+    },
+  },
+};
