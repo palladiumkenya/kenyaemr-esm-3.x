8b7b4b801fb3cd251e65b35263431a08708f145c
njorocs <njorocs@gmail.com>
2023-12-21 18:40:21 +0300

    (refactor) Tidy defaulter tracing

--- packages/esm-patient-clinical-view-app/src/config-schema.ts
+++ packages/esm-patient-clinical-view-app/src/config-schema.ts
@@ -15,6 +15,7 @@ export const configSchema = {
       antenatal: 'e8f98494-af35-4bb8-9fc7-c409c8fed843',
       postNatal: '72aa78e0-ee4b-47c3-9073-26f3b9ecc4a7',
       labourAndDelivery: '496c7cc3-0eea-4e84-a04c-2292949e2f7f',
+      defaulterTracingFormUuid: 'a1a62d1e-2def-11e9-b210-d663bd873d93',
     },
   },
   defaulterTracingEncounterUuid: {
@@ -26,6 +27,6 @@ export const configSchema = {
 
 export interface ConfigObject {
   encounterTypes: { mchMotherConsultation: string };
-  formsList: { labourAndDelivery: string; antenatal: string; postnatal: string };
+  formsList: { labourAndDelivery: string; antenatal: string; postnatal: string; defaulterTracingFormUuid: string };
   defaulterTracingEncounterUuid: string;
 }
