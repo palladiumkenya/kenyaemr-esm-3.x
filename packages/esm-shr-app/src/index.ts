78ed6c36b5da70121d0ae16ddc5e4242bb0018c1
jecihjoy <jecihjoy@gmail.com>
2024-02-04 00:13:33 +0300

    (feat) Community referrals initial commit

--- packages/esm-shr-app/src/index.ts
+++ packages/esm-shr-app/src/index.ts
@@ -3,6 +3,8 @@ import { configSchema } from './config-schema';
 import { dashboardMeta } from './dashboard.meta';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import shrPatientSummaryComponent from './shrpatient-summary/shrpatient-summary.component';
+import { createHomeDashboardLink } from './create-dashboard-link';
+import SHRRootComponent from './shr-root.component';
 
 const moduleName = '@kenyaemr/esm-shr-app';
 
@@ -10,6 +12,7 @@ const options = {
   featureName: 'patient-shr',
   moduleName,
 };
+
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 export const shrPatientSummary = getSyncLifecycle(shrPatientSummaryComponent, options);
@@ -21,3 +24,13 @@ export function startupApp() {
   registerBreadcrumbs([]);
   defineConfigSchema(moduleName, configSchema);
 }
+
+export const communityReferralsDashboardLink = getSyncLifecycle(
+  createHomeDashboardLink({
+    name: 'community-referrals',
+    title: 'Community referrals',
+  }),
+  options,
+);
+
+export const shrRoot = getSyncLifecycle(SHRRootComponent, options);
