087647f05a6dbeda223555613bfd8aa454dcb31f
Donald Kibet <chelashawdonald@yahoo.com>
2024-02-07 00:02:57 +0300

    (fix) update config object with description and type

--- packages/esm-billing-app/src/config-schema.ts
+++ packages/esm-billing-app/src/config-schema.ts
@@ -11,27 +11,32 @@ export interface BillingConfig {
 
 export const configSchema = {
   paymentDetails: {
-    type: Type.String,
-    default: '3b9dfac8-9e4d-11ee-8c90-0242ac120002',
+    _type: Type.String,
+    _default: '3b9dfac8-9e4d-11ee-8c90-0242ac120002',
   },
   paymentMethods: {
-    type: Type.String,
-    default: 'e6cb0c3b-04b0-4117-9bc6-ce24adbda802',
+    _type: Type.String,
+    _description: 'The payment methods visit attribute uuid',
+    _default: 'e6cb0c3b-04b0-4117-9bc6-ce24adbda802',
   },
   insuranceScheme: {
-    type: Type.String,
-    default: '2d0fa959-6780-41f1-85b1-402045935068',
+    _type: Type.String,
+    _description: 'The insurance scheme visit attribute uuid',
+    _default: '2d0fa959-6780-41f1-85b1-402045935068',
   },
   policyNumber: {
-    type: Type.String,
-    default: '0f4f3306-f01b-43c6-af5b-fdb60015cb02',
+    _type: Type.String,
+    _description: 'The policy number visit attribute uuid',
+    _default: '0f4f3306-f01b-43c6-af5b-fdb60015cb02',
   },
   patientCategory: {
-    type: Type.String,
-    default: 'df0362f9-782e-4d92-8bb2-3112e9e9eb3c',
+    _type: Type.String,
+    _description: 'The patient category visit attribute uuid',
+    _default: 'df0362f9-782e-4d92-8bb2-3112e9e9eb3c',
   },
   billPaymentStatus: {
-    type: Type.String,
-    default: '919b51c9-8e2e-468f-8354-181bf3e55786',
+    _type: Type.String,
+    _description: 'The bill payment status visit attribute uuid',
+    _default: '919b51c9-8e2e-468f-8354-181bf3e55786',
   },
 };
