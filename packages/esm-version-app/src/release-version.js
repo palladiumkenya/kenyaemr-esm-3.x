aaaafe4a538b1d9ee308e27001f0f05e25cf2644
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2024-04-08 15:05:10 +0300

    Stage 8: More mods

--- packages/esm-version-app/src/release-version.js
+++ packages/esm-version-app/src/release-version.js
@@ -1,16 +1,15 @@
-
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "v5.0.0.0-25-gbf8152b",
-    "hash": "gbf8152b",
-    "distance": 25,
-    "tag": "v5.0.0.0",
-    "semver": null,
-    "suffix": "25-gbf8152b",
-    "semverString": "",
-    "version": "5.1.0",
-    "buildDate": "2023-11-03T07:35:13.288Z"
+  dirty: false,
+  raw: 'v5.0.0.0-25-gbf8152b',
+  hash: 'gbf8152b',
+  distance: 25,
+  tag: 'v5.0.0.0',
+  semver: null,
+  suffix: '25-gbf8152b',
+  semverString: '',
+  version: '5.1.0',
+  buildDate: '2023-11-03T07:35:13.288Z',
 };
 /* tslint:enable */
