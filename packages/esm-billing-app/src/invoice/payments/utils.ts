804a6ba2b9d58ad4fca73337bf3248bf3a1cdfb1
its-kios09 <legacyitsolution@gmail.com>
2024-04-30 21:15:31 +0300

    (feat) added phone number formatter for formating the phone according to daraja api standards

--- packages/esm-billing-app/src/invoice/payments/utils.ts
+++ packages/esm-billing-app/src/invoice/payments/utils.ts
@@ -64,3 +64,22 @@ export const createPaymentPayload = (
 };
 
 const processBillItem = (item) => (item.item || item.billableService)?.split(':')[0];
+
+export function formatPhoneNumber(phone) {
+  let phone_ = phone.replace(/\D/g, '');
+  phone_ = phone_.toString();
+  const length = phone_.length;
+
+  let _phone = '';
+  if (length == 12 && phone_.substring(0, 3) == '254') {
+    _phone = phone_;
+  } else if (length == 9 && phone_.substring(0, 1) == 7) {
+    _phone = '254' + phone_;
+  } else if (length == 10 && phone_.substring(0, 1) == 0) {
+    _phone = '254' + phone_.substring(1, 10);
+  } else {
+    _phone = 'Invalid Phone Number ' + phone;
+  }
+
+  return _phone;
+}
