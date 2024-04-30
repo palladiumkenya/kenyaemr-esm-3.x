5a427d413296c63a554c065e613c6864c8efac95
its-kios09 <legacyitsolution@gmail.com>
2024-04-30 18:55:55 +0300

    (feat) generates the tokens for authenticating your stkpush api

---
+++ packages/esm-billing-app/src/invoice/payments/payment.resource.ts
@@ -0,0 +1,58 @@
+import useSWR from 'swr';
+import { openmrsFetch, useConfig } from '@openmrs/esm-framework';
+import { BillingConfig } from '../../config-schema';
+import { Buffer } from 'buffer';
+import axios from 'axios';
+
+type PaymentMethod = {
+  uuid: string;
+  description: string;
+  name: string;
+  retired: boolean;
+};
+
+const swrOption = {
+  errorRetryCount: 2,
+};
+
+export const usePaymentModes = () => {
+  const { excludedPaymentMode } = useConfig<BillingConfig>();
+  const url = `/ws/rest/v1/cashier/paymentMode`;
+  const { data, isLoading, error, mutate } = useSWR<{ data: { results: Array<PaymentMethod> } }>(
+    url,
+    openmrsFetch,
+    swrOption,
+  );
+  const allowedPaymentModes =
+    excludedPaymentMode?.length > 0
+      ? data?.data?.results.filter((mode) => !excludedPaymentMode.some((excluded) => excluded.uuid === mode.uuid)) ?? []
+      : data?.data?.results ?? [];
+  return {
+    paymentModes: allowedPaymentModes,
+    isLoading,
+    mutate,
+    error,
+  };
+};
+
+export const generateStkAccessToken = async () => {
+  try {
+    const consumerKey = '';
+    const consumerSecret = '';
+    const authorizationUrl = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+    const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${auth}`,
+    };
+    const response = await axios(authorizationUrl, { method: 'GET', headers });
+    const { access_token } = await response.data;
+    return access_token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const initiateStkPush = async (payload) => {
+  const access_token = await generateStkAccessToken();
+};
