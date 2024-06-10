74688a27b64ceb4e685aea56a67c263818b172f4
its-kios09 <legacyitsolution@gmail.com>
2024-06-11 01:19:49 +0300

    (feat) added initial notification error handling

--- packages/esm-billing-app/src/invoice/payments/payment.resource.ts
+++ packages/esm-billing-app/src/invoice/payments/payment.resource.ts
@@ -36,7 +36,7 @@ export const usePaymentModes = () => {
   };
 };
 
-export const generateStkAccessToken = async (authorizationUrl: string) => {
+export const generateStkAccessToken = async (authorizationUrl: string, setNotification) => {
   try {
     const consumerKey = '';
     const consumerSecret = '';
@@ -49,13 +49,14 @@ export const generateStkAccessToken = async (authorizationUrl: string) => {
     const { access_token } = await response.json();
     return access_token;
   } catch (error) {
+    setNotification('Unable to reach the MPESA server, please try again later.');
     throw error;
   }
 };
 
-export const initiateStkPush = async (payload, initiateUrl: string, authorizationUrl: string) => {
+export const initiateStkPush = async (payload, initiateUrl: string, authorizationUrl: string, setNotification) => {
   try {
-    const access_token = await generateStkAccessToken(authorizationUrl);
+    const access_token = await generateStkAccessToken(authorizationUrl, setNotification);
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${access_token}`,
@@ -65,8 +66,10 @@ export const initiateStkPush = async (payload, initiateUrl: string, authorizatio
       headers: headers,
       body: JSON.stringify(payload),
     });
+
     return await response.json();
   } catch (err) {
+    setNotification('Unable to initiate Lipa Na Mpesa, please try again later.');
     throw err;
   }
 };
