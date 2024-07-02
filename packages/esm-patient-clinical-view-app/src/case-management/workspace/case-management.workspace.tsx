17d97872e2fd2178aa237e743d44b69d5f31078e
its-kios09 <legacyitsolution@gmail.com>
2024-07-02 14:36:24 +0300

    (refactor) initial workspace form

--- packages/esm-patient-clinical-view-app/src/case-management/workspace/case-management.workspace.tsx
+++ packages/esm-patient-clinical-view-app/src/case-management/workspace/case-management.workspace.tsx
@@ -10,62 +10,74 @@ import {
   Row,
   ButtonSet,
   Button,
-  FilterableMultiSelect,
   MultiSelect,
   DatePicker,
   DatePickerInput,
+  Search,
 } from '@carbon/react';
 import styles from './case-management.scss';
-import { Controller } from 'react-hook-form';
+import { useSession } from '@openmrs/esm-framework';
 
 const CaseManagementForm: React.FC = () => {
   const { t } = useTranslation();
+  const { user } = useSession();
+  const currentProvider = user?.person?.display;
+  console.log(currentProvider);
   const onSubmit = async (data) => {};
-
   return (
-    <Form className={styles.form}>
-      <Stack gap={1} className={styles.grid}>
-        <span className={styles.caseFormTitle}>{t('formTitle', 'Fill in the form details')}</span>
-        <Column className={styles.formCaseColumn}>
-          <Layer className={styles.input}>
-            <TextInput id="provider_name" placeholder="Provider Name" labelText={t('provider', 'Provider Name')} />
-          </Layer>
+    <Form>
+      <span className={styles.caseFormTitle}>{t('formTitle', 'Fill in the form details')}</span>
+      <Stack gap={4} className={styles.grid}>
+        <span className={styles.sectionHeader}>Case Information</span>
+        <Column>
+          <Search
+            size="lg"
+            placeholder="Search for a case by name or identifier number"
+            labelText="Search"
+            closeButtonLabelText="Clear"
+            id="case-search"
+            onChange={() => {}}
+            onKeyDown={() => {}}
+          />
         </Column>
-        <Row className={styles.formCaseRow}>
-          <Column className={styles.formCaseColumn}>
-            <Layer className={styles.input}>
-              <DatePicker datePickerType="single">
-                <DatePickerInput
-                  placeholder="mm/dd/yyyy"
-                  labelText="Start Date"
-                  id="case-start-date-picker"
-                  size="md"
-                />
-              </DatePicker>
-            </Layer>
+        <span className={styles.sectionHeader}>Case Manager Information</span>
+
+        <Column>
+          <TextInput
+            id="case_manager_name"
+            placeholder="Case Manager Name"
+            labelText={t('manager', 'Case Manager Name')}
+          />
+        </Column>
+        <span className={styles.sectionHeader}> More Information</span>
+        <Row className={styles.datePickersRow}>
+          <Column>
+            <DatePicker datePickerType="single">
+              <DatePickerInput placeholder="mm/dd/yyyy" labelText="Start Date" id="case-start-date-picker" size="md" />
+            </DatePicker>
           </Column>
-          <Column className={styles.formCaseColumn}>
-            <Layer className={styles.input}>
-              <DatePicker datePickerType="single">
-                <DatePickerInput placeholder="mm/dd/yyyy" labelText="End Date" id="case-end-date-picker" size="md" />
-              </DatePicker>{' '}
-            </Layer>
+          <Column>
+            <DatePicker datePickerType="single">
+              <DatePickerInput placeholder="mm/dd/yyyy" labelText="End Date" id="case-end-date-picker" size="md" />
+            </DatePicker>
           </Column>
         </Row>
         <Column>
-          <Layer className={styles.input}>
-            <MultiSelect id="reasons" titleText={t('reasons', 'Reason for Assignment')} />
-          </Layer>
+          <MultiSelect id="reasons" titleText={t('reasons', 'Reason for Assignment')} />
         </Column>
-        <ButtonSet className={styles.buttonSet}>
-          <Button className={styles.button} kind="secondary">
-            {t('discardClaim', 'Discard Claim')}
-          </Button>
-          <Button className={styles.button} kind="primary" type="submit">
-            {t('processClaim', 'Process Claim')}
-          </Button>
-        </ButtonSet>
+        <Column className={styles.textbox}>
+          <TextArea labelText="Any additional notes" rows={4} id="case-manager-notes" />
+        </Column>
       </Stack>
+
+      <ButtonSet className={styles.buttonSet}>
+        <Button className={styles.button} kind="secondary">
+          {t('discard', 'Discard')}
+        </Button>
+        <Button className={styles.button} kind="primary" type="submit">
+          {t('submit', 'Submit')}
+        </Button>
+      </ButtonSet>
     </Form>
   );
 };
