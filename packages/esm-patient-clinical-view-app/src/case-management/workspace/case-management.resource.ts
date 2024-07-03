c740f8a898e5e094bd4df16f29255583630c5691
its-kios09 <legacyitsolution@gmail.com>
2024-07-03 14:55:14 +0300

    (feat) added relationshiptype hook

--- packages/esm-patient-clinical-view-app/src/case-management/workspace/case-management.resource.ts
+++ packages/esm-patient-clinical-view-app/src/case-management/workspace/case-management.resource.ts
@@ -9,9 +9,27 @@ interface CaseManager {
 interface CaseManagerResponse {
   results: CaseManager[];
 }
+
+interface RelationshipType {
+  uuid: string;
+  displayAIsToB: string;
+}
+
+interface RelationshipTypeResponse {
+  results: RelationshipType[];
+}
+
 export const useCaseManagers = () => {
   const customRepresentation = 'custom:(uuid,display)';
   const url = `/ws/rest/v1/provider?v=${customRepresentation}`;
+  const { data, error } = useSWRImmutable<{ data: CaseManagerResponse }>(url, openmrsFetch);
+
+  return { data, error };
+};
+
+export const useRelationshipType = () => {
+  const customRepresentation = 'custom:(uuid,display)';
+  const url = `/ws/rest/v1/relationshiptype?v=${customRepresentation}`;
   const { data, error } = useSWRImmutable<{ data: CaseManagerResponse }>(url, openmrsFetch);
 
   return { data, error };
