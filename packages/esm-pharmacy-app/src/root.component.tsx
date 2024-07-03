4ab169b54891a33e9a06cb8b6f80eb77585c4739
Laurent Ouma <lawiomosh3@gmail.com>
2024-07-03 08:37:55 +0300

    Added Pharmacy detail for listing pharmacy users and patients

--- packages/esm-pharmacy-app/src/root.component.tsx
+++ packages/esm-pharmacy-app/src/root.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PharmacyComponent from './pharmacy-component/pharmacy.component';
+import PharmacyDetail from './pharmacy-detail/pharmacy-detail.component';
 
 const Root: React.FC = () => {
   const baseName = window.getOpenmrsSpaBase() + 'home/pharmacy';
@@ -9,6 +10,7 @@ const Root: React.FC = () => {
     <BrowserRouter basename={baseName}>
       <Routes>
         <Route path="/" element={<PharmacyComponent />} />
+        <Route path="/:pharmacyUuid" element={<PharmacyDetail />} />
       </Routes>
     </BrowserRouter>
   );
