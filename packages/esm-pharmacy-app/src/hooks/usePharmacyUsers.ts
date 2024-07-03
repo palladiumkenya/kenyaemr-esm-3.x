21ec5c0454b04b0af0819bc364046d007cba3caa
Laurent Ouma <lawiomosh3@gmail.com>
2024-07-03 09:59:15 +0300

    Implemented phamacy patients and user tables and hook, patients and users not pharmacy assigned

--- packages/esm-pharmacy-app/src/hooks/usePharmacyUsers.ts
+++ packages/esm-pharmacy-app/src/hooks/usePharmacyUsers.ts
@@ -1,3 +1,28 @@
-const usePharmacyUsers = (pharmacyUuid: string) => {};
+import { FetchResponse, Person, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
+import useSWR from 'swr';
+import { PharmacyUser } from '../types';
+
+const usePharmacyUsers = (pharmacyUuid: string) => {
+  const customPresentation = 'full';
+  const url = `${restBaseUrl}/person/?q=&limit=7&v=${customPresentation}`;
+
+  const { data, isLoading, error } = useSWR<FetchResponse<{ results: Person[] }>>(url, openmrsFetch);
+
+  return {
+    isLoading,
+    error,
+    users: (data?.data?.results ?? []).map(extractData),
+  };
+};
+
+const extractData = (person: Person) => {
+  return {
+    name: person.preferredName.display,
+    uuid: person.uuid,
+    age: person.age,
+    gender: person.gender,
+    telephoneContact: '0787687656',
+  } as PharmacyUser;
+};
 
 export default usePharmacyUsers;
