d24104ada5db681096c6c63460a4bae214860766
Laurent Ouma <lawiomosh3@gmail.com>
2024-07-04 11:40:19 +0300

    Fetching actuall assigned pharmacy for session user, fetching actual users and patients for selected pharmacy, some attributes still missing

--- packages/esm-pharmacy-app/src/hooks/usePharmacyPatients.ts
+++ packages/esm-pharmacy-app/src/hooks/usePharmacyPatients.ts
@@ -1,27 +1,47 @@
-import { FetchResponse, Patient, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
+import { FetchResponse, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
 import useSWR from 'swr';
-import { PharmacyPatient } from '../types';
+import { PharamacyPatientMapping, PharmacyPatient } from '../types';
 
 const usePharmacyPatients = (pharmacyUuid: string) => {
   const customPresentation = 'full';
-  const url = `${restBaseUrl}/patient/?q=123&limit=7&v=${customPresentation}`;
+  const url = `${restBaseUrl}/datafilter/search`;
 
-  const { data, isLoading, error } = useSWR<FetchResponse<{ results: Patient[] }>>(url, openmrsFetch);
+  const fetchPharmacyPatients = async (url: string) => {
+    const abortController = new AbortController();
+
+    const response = await openmrsFetch(url, {
+      body: JSON.stringify({
+        entityIdentifier: '',
+        entityType: 'org.openmrs.Patient',
+        basisIdentifier: pharmacyUuid,
+        basisType: 'org.openmrs.Location',
+      }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      signal: abortController.signal,
+    });
+    return response;
+  };
+
+  const { data, isLoading, error } = useSWR<FetchResponse<PharamacyPatientMapping[]>>(url, fetchPharmacyPatients);
 
   return {
     isLoading,
     error,
-    patients: (data?.data?.results ?? []).map(extractData),
+    patients: (data?.data ?? []).map(extractData),
   };
 };
 
-const extractData = (patient: Patient) => {
+const extractData = (mapping: PharamacyPatientMapping) => {
   return {
-    name: patient.person.preferredName.display,
-    uuid: patient.uuid,
-    age: patient.person.age,
-    gender: patient.person.gender,
+    name: mapping.entity.name,
+    uuid: mapping.entity.uuid,
+    age: mapping.entity.age,
+    gender: mapping.entity.gender,
     telephoneContact: '0787687656',
+    openmrsId: mapping.entity.OpenMRSID,
   } as PharmacyPatient;
 };
 
