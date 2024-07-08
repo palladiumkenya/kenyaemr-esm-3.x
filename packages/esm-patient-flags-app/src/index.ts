bf1c0ae6d75744c9f3395d517b6645e4d4fb74ba
Makombe Kennedy <kennedymakombe@gmail.com>
2024-07-08 15:42:29 +0300

    Remove the link to 2x-chart (#264)

--- packages/esm-patient-flags-app/src/index.ts
+++ packages/esm-patient-flags-app/src/index.ts
@@ -1,6 +1,5 @@
 import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
-import kenyaEmr2ChartLinkComponent from './kenyaemr-link/kenyaemr-chart-link.component';
 import patientFlagsComponent from './patient-flags/patient-flags.component';
 import NavbarActionButton from './navbar/navbar-action-button.component';
 
@@ -13,7 +12,6 @@ const options = {
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 export const navbarButtons = getSyncLifecycle(NavbarActionButton, options);
-export const kenyaemr2ChartLink = getSyncLifecycle(kenyaEmr2ChartLinkComponent, options);
 export const patientFlag = getSyncLifecycle(patientFlagsComponent, options);
 
 export function startupApp() {
