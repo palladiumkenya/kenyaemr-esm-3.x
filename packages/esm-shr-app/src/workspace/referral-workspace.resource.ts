d2ba98d9c4b369a10e65d49306d30bf252e5951d
its-kios09 <legacyitsolution@gmail.com>
2024-07-09 21:31:07 +0300

    (refactor) limit of query from 10 to 15

--- packages/esm-shr-app/src/workspace/referral-workspace.resource.ts
+++ packages/esm-shr-app/src/workspace/referral-workspace.resource.ts
@@ -17,7 +17,7 @@ export interface ReasonResponse {
 
 export const useReason = (searchTerm: string) => {
   const customRepresentation = 'custom:(uuid,name)';
-  const url = `/ws/rest/v1/concept?v=${customRepresentation}&q=${searchTerm}&limit=10`;
+  const url = `/ws/rest/v1/concept?v=${customRepresentation}&q=${searchTerm}&limit=15`;
   const { data, error } = useSWRImmutable<{ data: ReasonResponse }>(searchTerm ? url : null, openmrsFetch);
 
   return { data: data?.data?.results, error };
