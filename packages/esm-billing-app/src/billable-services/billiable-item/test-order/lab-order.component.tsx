bad8704499d161175d715101a1bc841f9ce085cf
its-kios09 <legacyitsolution@gmail.com>
2024-07-13 00:44:48 +0300

    (feat) added a UI for the showing prices

--- packages/esm-billing-app/src/billable-services/billiable-item/test-order/lab-order.component.tsx
+++ packages/esm-billing-app/src/billable-services/billiable-item/test-order/lab-order.component.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { convertToCurrency } from '../../../helpers';
 import { useBillableItem } from '../useBillableItem';
+import { useTranslation } from 'react-i18next';
+import styles from './lab-oder.scss';
+import {
+  InlineNotification,
+  StructuredListWrapper,
+  StructuredListHead,
+  StructuredListRow,
+  StructuredListCell,
+  StructuredListBody,
+  Tile,
+} from '@carbon/react';
 
 type LabOrderProps = {
   order: {
@@ -12,23 +23,58 @@ type LabOrderProps = {
 };
 
 const LabOrder: React.FC<LabOrderProps> = ({ order }) => {
-  // TODO: Implement logic to display whether the lab order service is available to ensure clinicians can order the service
-
-  const { billableItem, error, isLoading } = useBillableItem(order?.testType?.conceptUuid);
-
-  const billItems = billableItem?.servicePrices
-    .map((servicePrice) => `${servicePrice?.paymentMode?.name} - ${convertToCurrency(servicePrice?.price)}`)
-    .join(' ');
+  const { t } = useTranslation();
+  const { billableItem, isLoading, error } = useBillableItem(order?.testType?.conceptUuid);
 
   if (isLoading) {
-    return null;
+    return (
+      <InlineNotification
+        kind="info"
+        title={t('loadingPrices', 'Loading prices')}
+        subtitle={t('pleaseWait', 'Please wait...')}
+        lowContrast
+      />
+    );
   }
 
-  if (error) {
-    return null;
+  if (error || !billableItem) {
+    return (
+      <InlineNotification
+        kind="info"
+        title={t('noprices', 'No procedural price found ')}
+        subtitle={t('noInfo', 'Please contact the cashier')}
+        lowContrast
+      />
+    );
   }
 
-  return <p>{billItems}</p>;
+  return (
+    <Tile id="" className={styles.prices}>
+      <div className={styles.listContainer}>
+        <StructuredListWrapper isCondensed>
+          A
+          <StructuredListHead>
+            <StructuredListRow head>
+              <StructuredListCell head className={styles.cell}>
+                {t('paymentMethods', 'Payment methods')}
+              </StructuredListCell>
+              <StructuredListCell head className={styles.cell}>
+                {t('prices', 'Prices(Ksh)')}
+              </StructuredListCell>
+            </StructuredListRow>
+          </StructuredListHead>
+          <StructuredListBody>
+            {billableItem.servicePrices.map((priceItem) => (
+              <StructuredListRow key={priceItem.uuid}>
+                <StructuredListCell className={styles.cell}>{priceItem.paymentMode.name}</StructuredListCell>
+                <StructuredListCell className={styles.cell}>{convertToCurrency(priceItem.price)}</StructuredListCell>
+              </StructuredListRow>
+            ))}
+          </StructuredListBody>
+        </StructuredListWrapper>
+      </div>
+    </Tile>
+  );
 };
 
 export default LabOrder;
