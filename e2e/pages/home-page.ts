c2b6471c10a2b8b08903edaad327dd7690bd4f27
Amos Machora <amosmachora13@gmail.com>
2024-07-24 18:39:06 +0300

    tests: fixed broken tests by adding page change event listener and redirecting accordingly

--- e2e/pages/home-page.ts
+++ e2e/pages/home-page.ts
@@ -6,19 +6,4 @@ export class HomePage {
   async gotoHome() {
     await this.page.goto('home');
   }
-
-  async selectLocationIfPresent() {
-    if (await this.page.locator('text=Select your location from the list below.').isVisible()) {
-      const firstCheckbox = this.page.locator('input[type="radio"]').first();
-      await firstCheckbox.check();
-      await this.page.locator('button:has-text("Confirm")').click();
-    }
-  }
-}
-
-export class BillingPage {
-  constructor(readonly page: Page) {}
-  async gotoBilling() {
-    await this.page.goto('/openmrs/spa/home/billing');
-  }
 }
--- e2e/specs/login-test.spec.ts
+++ e2e/specs/login-test.spec.ts
@@ -7,13 +7,6 @@ test('Go to homepage', async ({ page }) => {
 
   await test.step('When I visit the home page', async () => {
     await homePage.gotoHome();
-  });
-
-  await test.step('If redirected to select login location, choose location', async () => {
-    await homePage.selectLocationIfPresent();
-  });
-
-  await test.step('Then should be at the home page', async () => {
     await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/home`);
   });
 });
