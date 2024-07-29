2bbc92d71076f81a717305105c1201db76c2c81c
Rugute <rrugute@gmail.com>
2024-07-29 10:27:53 +0300

    sorted out Providers esm

---
+++ packages/esm-providers-app/src/api/api.ts
@@ -0,0 +1,40 @@
+import useSWR from 'swr';
+import { Buffer } from 'buffer';
+import { openmrsFetch, restBaseUrl, useConfig } from '@openmrs/esm-framework';
+
+export const BASE_URL = '/ws/rest/v1/provider?v=full';
+export const custom =
+  '/ws/rest/v1/provider?&v=custom:(uuid,identifier,display,person:(uuid,display),attributes:(uuid,display),retired)';
+
+const fetcher = async (url) => {
+  try {
+    const response = await openmrsFetch(url, {});
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    throw new Error(`An error occurred while fetching data: ${error.message}`);
+  }
+};
+
+export const getAllProviders = () => {
+  const { data, isLoading, error, isValidating } = useSWR(`${custom}`, fetcher);
+
+  const response = data ? (data as any)?.results : [];
+
+  return {
+    response,
+    isLoading,
+    error,
+    isValidating,
+  };
+};
+
+export const searchUsers = async (name: string, ac = new AbortController()) => {
+  const results = await openmrsFetch(`ws/rest/v1/user?q=${name}&v=custom:(uuid,display,person)`, {
+    signal: ac.signal,
+  });
+  return results.data.results;
+};
---
+++ packages/esm-providers-app/src/index.ts
@@ -0,0 +1,34 @@
+import { getAsyncLifecycle, defineConfigSchema, getSyncLifecycle, registerBreadcrumbs } from '@openmrs/esm-framework';
+import { configSchema } from './config-schema';
+import { createLeftPanelLink } from './providers-left-panel/providers-left-panel-link.component';
+const moduleName = '@kenyaemr/esm-providers-app';
+
+const options = {
+  featureName: 'esm-providers-app',
+  moduleName,
+};
+
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+
+export function startupApp() {
+  const morgueBasepath = `${window.spaBase}/home/providers`;
+
+  defineConfigSchema(moduleName, configSchema);
+  registerBreadcrumbs([
+    {
+      title: 'providers',
+      path: morgueBasepath,
+      parent: `${window.spaBase}/home`,
+    },
+  ]);
+}
+
+export const root = getAsyncLifecycle(() => import('./root.component'), options);
+
+export const providersDashboardLink = getSyncLifecycle(
+  createLeftPanelLink({
+    name: 'providers',
+    title: 'Providers',
+  }),
+  options,
+);
