67a61aab44bcc2e4eed41e57ac6e782e3ee8079c
its-kios09 <legacyitsolution@gmail.com>
2024-08-01 19:31:42 +0300

    (feat) added search component

--- packages/esm-morgue-app/src/tables/generic-table.component.tsx
+++ packages/esm-morgue-app/src/tables/generic-table.component.tsx
@@ -10,64 +10,84 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  Search,
 } from '@carbon/react';
-import { PatientChartPagination } from '@openmrs/esm-patient-common-lib';
+import { CardHeader, PatientChartPagination } from '@openmrs/esm-patient-common-lib';
 import { useLayoutType, usePagination } from '@openmrs/esm-framework';
 import styles from './generic-table.scss';
 
 interface GenericTableProps {
   rows: any[];
   headers: { header: string; key: string }[];
+  actionColumn?: (row: any) => React.ReactNode;
+  title?: string;
 }
 
-const GenericTable: React.FC<GenericTableProps> = ({ rows, headers }) => {
+const GenericTable: React.FC<GenericTableProps> = ({ rows, headers, actionColumn, title }) => {
   const { t } = useTranslation();
-  const pageSize = 10; // Set your desired page size
+  const pageSize = 10;
   const { results: paginatedData, currentPage, goTo } = usePagination(rows, pageSize);
   const isTablet = useLayoutType() === 'tablet';
 
   return (
-    <div>
-      <DataTable rows={paginatedData} headers={headers} isSortable size={isTablet ? 'lg' : 'sm'} useZebraStyles>
-        {({ rows, headers, getHeaderProps, getTableProps }) => (
-          <TableContainer>
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader
-                      key={header.key}
-                      className={classNames(styles.productiveHeading01, styles.text02)}
-                      {...getHeaderProps({
-                        header,
-                        isSortable: header.isSortable,
-                      })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.id}>
-                    {row.cells.map((cell) => (
-                      <TableCell key={cell.id}>{cell.value}</TableCell>
+    <>
+      <CardHeader title={title} children={''} />
+      <div className={styles.searchContainer}>
+        <Search
+          size="sm"
+          placeholder="Search for deceased"
+          labelText="Search"
+          closeButtonLabelText="Clear search input"
+          id="search-deceased"
+          onChange={() => {}}
+          onKeyDown={() => {}}
+          className={styles.searchInput}
+        />
+      </div>
+      <div className={styles.genericTable}>
+        <DataTable rows={paginatedData} headers={headers} isSortable size={isTablet ? 'lg' : 'sm'} useZebraStyles>
+          {({ rows, headers, getHeaderProps, getTableProps }) => (
+            <TableContainer className={styles.tableContainer}>
+              <Table {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header) => (
+                      <TableHeader
+                        key={header.key}
+                        className={classNames(styles.productiveHeading01, styles.text02)}
+                        {...getHeaderProps({
+                          header,
+                          isSortable: header.isSortable,
+                        })}>
+                        {header.header}
+                      </TableHeader>
                     ))}
+                    {actionColumn && <TableHeader>Action</TableHeader>}
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-      </DataTable>
-      <PatientChartPagination
-        currentItems={paginatedData.length}
-        totalItems={rows.length}
-        onPageNumberChange={goTo}
-        pageNumber={currentPage}
-        pageSize={pageSize}
-      />
-    </div>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.id}>
+                      {row.cells.map((cell) => (
+                        <TableCell key={cell.id}>{cell.value}</TableCell>
+                      ))}
+                      {actionColumn && <TableCell>{actionColumn(row)}</TableCell>}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </DataTable>
+        <PatientChartPagination
+          currentItems={paginatedData.length}
+          totalItems={rows.length}
+          onPageNumberChange={goTo}
+          pageNumber={currentPage}
+          pageSize={pageSize}
+        />
+      </div>
+    </>
   );
 };
 
--- packages/esm-morgue-app/src/tables/waiting-queue.component.tsx
+++ packages/esm-morgue-app/src/tables/waiting-queue.component.tsx
@@ -3,36 +3,46 @@ import GenericTable from './generic-table.component';
 import { useDeceasedPatient } from './generic-table.resource';
 import { formatDate } from '@openmrs/esm-framework';
 import { toUpperCase } from '../helpers/expression-helper';
+import { Tag, Button } from '@carbon/react';
+import styles from './generic-table.scss';
+import { useTranslation } from 'react-i18next';
 
 export const WaitingQueue: React.FC = () => {
   const { data: deceasedPatients, error, isLoading } = useDeceasedPatient('Test');
+  const { t } = useTranslation();
+  const fromHospital = t('fromHosp', 'From Hospital');
 
   const genericTableHeader = [
+    { header: 'S/No', key: 'serialNumber' },
     { header: 'Deceased Name', key: 'name' },
-    { header: 'ID No ', key: 'identification' },
+    { header: 'ID No ', key: 'Ids' },
     { header: 'Gender', key: 'gender' },
     { header: 'Dob', key: 'birthdate' },
     { header: 'Date of Death', key: 'deathDate' },
     { header: 'Cause of Death', key: 'causeOfDeath' },
     { header: 'Status Death', key: 'status' },
-    { header: 'Queue', key: 'queue' },
-    { header: 'Body Identification', key: 'identify' },
     { header: 'Residence', key: 'address4' },
   ];
 
   const rows =
-    deceasedPatients?.map((patient) => ({
+    deceasedPatients?.map((patient, index) => ({
       id: patient.uuid,
+      serialNumber: index + 1,
       name: toUpperCase(patient.person.display),
       gender: patient.person.gender,
       birthdate: formatDate(new Date(patient.person.birthdate)),
       deathDate: formatDate(new Date(patient.person.deathDate)),
       causeOfDeath: patient.person.causeOfDeath?.display || 'N/A',
-      status: patient.person.dead ? 'Deceased' : 'Alive',
-      queue: 'N/A', // Add queue data if available
-      identification: patient.identifiers[0]?.identifier || 'N/A',
+      status: <Tag type="green">{patient.person.dead ? 'Known' : 'Unknown'}</Tag>,
+      Ids: patient.identifiers[0]?.identifier || 'N/A',
       address4: patient.person.preferredAddress?.address4 || 'N/A',
     })) || [];
 
-  return <GenericTable rows={rows} headers={genericTableHeader} />;
+  const actionColumn = () => (
+    <Button kind="tertiary" className={styles.actionBtn}>
+      {t('readyForAdmit', 'Ready for Admission')}
+    </Button>
+  );
+
+  return <GenericTable rows={rows} headers={genericTableHeader} actionColumn={actionColumn} title={fromHospital} />;
 };
