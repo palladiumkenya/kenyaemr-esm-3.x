2ff919fde1a8271422ab4a9c82624d546af2d144
Donald Kibet <chelashawdonald@yahoo.com>
2024-08-12 16:07:32 +0300

    (feat) : Add HEI (Health Information Exchange) provider management dashboard (#305)
    * sorted out Providers esm
    
    * Update packages/esm-providers-app/src/index.ts
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/providers-header/providers-header.scss
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/providers-metrics/providers-metrics-header.component.tsx
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/providers-metrics/providers-metrics-header.component.tsx
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/providers-tables/providers-discharged.component.tsx
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/api/api.ts
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * Update packages/esm-providers-app/src/api/api.ts
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>
    
    * (fix) : fix linting and prettier errors
    
    * (fix) : ensure correct addition of providers app
    
    ---------
    
    Co-authored-by: Rugute <rrugute@gmail.com>

---
+++ packages/esm-providers-app/src/providers-left-panel/providers-left-panel-link.component.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo } from 'react';
+import last from 'lodash-es/last';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import { ConfigurableLink } from '@openmrs/esm-framework';
+
+export interface LinkConfig {
+  name: string;
+  title: string;
+}
+
+export function LinkExtension({ config }: { config: LinkConfig }) {
+  const { name, title } = config;
+  const location = useLocation();
+  const spaBasePath = window.getOpenmrsSpaBase() + 'home';
+
+  let urlSegment = useMemo(() => decodeURIComponent(last(location.pathname.split('/'))), [location.pathname]);
+
+  const isUUID = (value) => {
+    const regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+    return regex.test(value);
+  };
+
+  if (isUUID(urlSegment)) {
+    urlSegment = 'providers';
+  }
+
+  return (
+    <ConfigurableLink
+      to={spaBasePath + '/' + name}
+      className={`cds--side-nav__link ${name === urlSegment && 'active-left-nav-link'}`}>
+      {title}
+    </ConfigurableLink>
+  );
+}
+
+export const createLeftPanelLink = (config: LinkConfig) => () =>
+  (
+    <BrowserRouter>
+      <LinkExtension config={config} />
+    </BrowserRouter>
+  );
--- packages/esm-version-app/src/release-version.js
+++ packages/esm-version-app/src/release-version.js
@@ -1,16 +1,28 @@
-
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "v5.1.1.1-47-gd09df8f",
-    "hash": "gd09df8f",
-    "distance": 47,
-    "tag": "v5.1.1.1",
-    "semver": null,
-    "suffix": "47-gd09df8f",
-    "semverString": "",
-    "version": "5.1.1",
-    "buildDate": "2024-06-11T13:15:27.958Z"
+  dirty: true,
+  raw: 'v5.2.0-48-gcaa24f6-dirty',
+  hash: 'gcaa24f6',
+  distance: 48,
+  tag: 'v5.2.0',
+  semver: {
+    options: {
+      loose: false,
+      includePrerelease: false,
+    },
+    loose: false,
+    raw: 'v5.2.0',
+    major: 5,
+    minor: 2,
+    patch: 0,
+    prerelease: [],
+    build: [],
+    version: '5.2.0',
+  },
+  suffix: '48-gcaa24f6-dirty',
+  semverString: '5.2.0+48.gcaa24f6',
+  version: '5.2.0',
+  buildDate: '2024-08-12T12:12:16.959Z',
 };
 /* tslint:enable */
