dad2ee3c70b6287d761865f1313524b76e75a67e
1wes <okemwawes@gmail.com>
2024-08-14 22:18:14 +0300

    fix: push file to prevent linting error on missing imported module

---
+++ packages/esm-billing-app/src/billable-services/manage-commodity-price/manage-commodity-price.component.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Tab, Tabs, TabList } from '@carbon/react';
+
+const ManageCommodityPrices = () => {
+  const [activeTabIndex, setActiveTabindex] = useState<number>(0);
+
+  const { t } = useTranslation();
+
+  const handleTabChange = ({ selectedIndex }: { selectedIndex: number }) => {
+    setActiveTabindex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Tabs onChange={handleTabChange} selectedIndex={activeTabIndex}>
+        <TabList>
+          <Tab>{'Search'}</Tab>
+        </TabList>
+      </Tabs>
+    </div>
+  );
+};
+export default ManageCommodityPrices;
