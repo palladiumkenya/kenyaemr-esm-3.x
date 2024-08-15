32815630ff00696eea8f9e083828926a08d42de7
Laurent Ouma <lawiomosh3@gmail.com>
2024-08-16 00:10:11 +0300

    Using SHR json moch data for shr summary

--- packages/esm-shr-app/src/hooks/useSHRSummary.ts
+++ packages/esm-shr-app/src/hooks/useSHRSummary.ts
@@ -1,16 +1,23 @@
 import { openmrsFetch } from '@openmrs/esm-framework';
 import useSWR from 'swr';
 import { SHRSummary } from '../types/index';
+import { useState } from 'react';
 
 export const useSHRSummary = (patientUuid: string) => {
-  const shrSummaryUrl = `/ws/rest/v1/kenyaemril/shrPatientSummary?patientUuid=${patientUuid}`;
-  const { data, mutate, error, isLoading } = useSWR<{ data: SHRSummary }>(shrSummaryUrl, openmrsFetch);
+  // const shrSummaryUrl = `/ws/rest/v1/kenyaemril/shrPatientSummary?patientUuid=${patientUuid}`;
+  // const { data, mutate, error, isLoading } = useSWR<{ data: SHRSummary }>(shrSummaryUrl, openmrsFetch);
+  // return {
+  //   data: data?.data ? data?.data : null,
+  //   isError: undefined,
+  //   isLoading: isLoading,
+  // };
+  const [state, setState] = useState({ isError: undefined, isLoading: true, data: require('./visits.json') });
 
-  return {
-    data: data?.data ? data?.data : null,
-    isError: error,
-    isLoading: isLoading,
-  };
+  setTimeout(() => {
+    setState({ ...state, isLoading: false });
+  }, 3000);
+
+  return state;
 };
 
 export const useCommunityReferrals = (status: string) => {
