695cbc2d3f34a0673d95030ae0f7bd1cb1a0beb4
Laurent Ouma <lawiomosh3@gmail.com>
2024-08-19 00:05:56 +0300

    Removed feature flag

--- packages/esm-billing-app/src/benefits-package/benefits-package.component.tsx
+++ packages/esm-billing-app/src/benefits-package/benefits-package.component.tsx
@@ -1,32 +1,8 @@
-import { Layer, Tile } from '@carbon/react';
-import { useFeatureFlag } from '@openmrs/esm-framework';
-import { CardHeader, EmptyDataIllustration } from '@openmrs/esm-patient-common-lib';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import BenefitsHeader from './benefits-header.components';
-import styles from './table/benebfits-table.scss';
 import BenefitsTable from './table/benefits-table.component';
 
 const BenefitsPackage = () => {
-  const isBenefitsPackageEnabled = useFeatureFlag('benefits-package');
-  const { t } = useTranslation();
-  const headerTitle = t('benefits', 'Benefits');
-  if (!isBenefitsPackageEnabled) {
-    return (
-      <div>
-        <Layer>
-          <Tile className={styles.tile}>
-            <CardHeader title={headerTitle}>{''}</CardHeader>
-            <EmptyDataIllustration />
-            <p className={styles.content}>
-              {t('unavailable', 'Benefits package feature is stil under active developement')}
-            </p>
-          </Tile>
-        </Layer>
-      </div>
-    );
-  }
-
   return (
     <div className={`omrs-main-content `}>
       <BenefitsHeader />
