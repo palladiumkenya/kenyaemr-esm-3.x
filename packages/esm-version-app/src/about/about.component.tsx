183965679929b5e4030714ba8b44756cafb3c07e
Laurent Ouma <lawiomosh3@gmail.com>
2024-08-23 16:34:52 +0300

    Added SHA acreditation information on system information

--- packages/esm-version-app/src/about/about.component.tsx
+++ packages/esm-version-app/src/about/about.component.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from './about.scss';
 import { useModules } from '../hooks/useModules';
 import { useDefaultFacility, useSystemSetting } from '../hooks/useSystemSetting';
-import { formatDatetime } from '@openmrs/esm-framework';
+import { formatDate, formatDatetime } from '@openmrs/esm-framework';
 import FrontendModule from '../frontend-modules/frontend-modules.component';
+import { SkeletonText } from '@carbon/react';
+import dayjs from 'dayjs';
 const packageInfo = require('../release-version.js');
 
 interface AboutProps {}
@@ -13,9 +15,9 @@ const About: React.FC<AboutProps> = () => {
   const kenyaEMR = modules.find(({ uuid }) => uuid === 'kenyaemr');
   const { mflCodeResource } = useSystemSetting('facility.mflcode');
   const mflCode = mflCodeResource ? `(${mflCodeResource?.value ?? ''})` : '';
-  const facilityName = useDefaultFacility();
   const { title, container, aboutBody, aboutPage } = styles;
   const { VERSION } = packageInfo;
+  const { defaultFacility, error, isLoading: defaultFacilityLoading } = useDefaultFacility();
 
   return (
     <div className={styles.main}>
@@ -42,6 +44,18 @@ const About: React.FC<AboutProps> = () => {
             <p>{formatDatetime(new Date(VERSION.buildDate), { mode: 'standard' })}</p>
             <p>Facility code</p>
             <p>{mflCode ?? '--'}</p>
+            <p>Facility Name</p>
+            {defaultFacilityLoading ? <SkeletonText /> : <p>{defaultFacility?.display}</p>}
+            <p>Operational status</p>
+            {defaultFacilityLoading ? <SkeletonText /> : <p>{defaultFacility?.operationalStatus}</p>}
+            <p>Sha Contracted</p>
+            {defaultFacilityLoading ? <SkeletonText /> : <p>{defaultFacility?.shaContracted}</p>}
+            <p>Expiry Date</p>
+            {defaultFacilityLoading ? (
+              <SkeletonText />
+            ) : (
+              <p>{defaultFacility?.expiryDate ?? formatDate(dayjs().add(10, 'days').toDate())}</p>
+            )}
           </div>
         </div>
       </div>
--- packages/esm-version-app/src/types/index.ts
+++ packages/esm-version-app/src/types/index.ts
@@ -2,3 +2,12 @@ export interface FrontendModule {
   name: string;
   version?: string;
 }
+
+export interface DefaultFacility {
+  locationId: number;
+  uuid: string;
+  display: string;
+  operationalStatus: string;
+  shaContracted: string;
+  expiryDate: string;
+}
