44e39564cbcf3a786fc27ad3a52fad8249c99890
Laurent Ouma <lawiomosh3@gmail.com>
2024-08-28 16:08:26 +0300

    Cleaned up SHR Code, Requiring SHA Number before pulling patient info

---
+++ packages/esm-shr-app/src/constants.ts
@@ -0,0 +1,6 @@
+export const PHONE_NUMBER_REGEX = /^(\+?254|0)((7|1)\d{8})$/;
+export const AUTH_TYPES = [
+  { value: 'otp', label: 'OTP' },
+  { value: 'pin', label: 'PIN' },
+  { value: 'biometrics', label: 'BIOMETRICS' },
+];
