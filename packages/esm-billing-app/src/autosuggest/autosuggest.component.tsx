c97ee6cf8a37657be3ffc251950651e9c76ee13c
Laurent Ouma <lawiomosh3@gmail.com>
2024-09-03 16:28:35 +0300

    Enabling choosing of method and pricing for billable services.Also form state managed with react hook form, validated , cleaned and optimized

---
+++ packages/esm-billing-app/src/autosuggest/autosuggest.component.tsx
@@ -0,0 +1,187 @@
+import React, { type HTMLAttributes, useEffect, useRef, useState } from 'react';
+import { Layer, Search, type SearchProps } from '@carbon/react';
+import classNames from 'classnames';
+import styles from './autosuggest.scss';
+
+// FIXME Temporarily included types from Carbon
+type InputPropsBase = Omit<HTMLAttributes<HTMLInputElement>, 'onChange'>;
+
+interface SearchProps extends InputPropsBase {
+  /**
+   * Specify an optional value for the `autocomplete` property on the underlying
+   * `<input>`, defaults to "off"
+   */
+  autoComplete?: string;
+
+  /**
+   * Specify an optional className to be applied to the container node
+   */
+  className?: string;
+
+  /**
+   * Specify a label to be read by screen readers on the "close" button
+   */
+  closeButtonLabelText?: string;
+
+  /**
+   * Optionally provide the default value of the `<input>`
+   */
+  defaultValue?: string | number;
+
+  /**
+   * Specify whether the `<input>` should be disabled
+   */
+  disabled?: boolean;
+
+  /**
+   * Specify whether or not ExpandableSearch should render expanded or not
+   */
+  isExpanded?: boolean;
+
+  /**
+   * Specify a custom `id` for the input
+   */
+  id?: string;
+
+  /**
+   * Provide the label text for the Search icon
+   */
+  labelText: React.ReactNode;
+
+  /**
+   * Optional callback called when the search value changes.
+   */
+  onChange?(e: { target: HTMLInputElement; type: 'change' }): void;
+
+  /**
+   * Optional callback called when the search value is cleared.
+   */
+  onClear?(): void;
+
+  /**
+   * Optional callback called when the magnifier icon is clicked in ExpandableSearch.
+   */
+  onExpand?(e: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>): void;
+
+  /**
+   * Provide an optional placeholder text for the Search.
+   * Note: if the label and placeholder differ,
+   * VoiceOver on Mac will read both
+   */
+  placeholder?: string;
+
+  /**
+   * Rendered icon for the Search.
+   * Can be a React component class
+   */
+  renderIcon?: React.ComponentType | React.FunctionComponent;
+
+  /**
+   * Specify the role for the underlying `<input>`, defaults to `searchbox`
+   */
+  role?: string;
+
+  /**
+   * Specify the size of the Search
+   */
+  size?: 'sm' | 'md' | 'lg';
+
+  /**
+   * Optional prop to specify the type of the `<input>`
+   */
+  type?: string;
+
+  /**
+   * Specify the value of the `<input>`
+   */
+  value?: string | number;
+}
+
+interface AutosuggestProps extends SearchProps {
+  getDisplayValue: Function;
+  getFieldValue: Function;
+  getSearchResults: (query: string) => Promise<any>;
+  onSuggestionSelected: (field: string, value: string) => void;
+  invalid?: boolean | undefined;
+  invalidText?: string | undefined;
+}
+
+export const Autosuggest: React.FC<AutosuggestProps> = ({
+  getDisplayValue,
+  getFieldValue,
+  getSearchResults,
+  onSuggestionSelected,
+  invalid,
+  invalidText,
+  ...searchProps
+}) => {
+  const [suggestions, setSuggestions] = useState([]);
+  const searchBox = useRef(null);
+  const wrapper = useRef(null);
+  const { id: name, labelText } = searchProps;
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutsideComponent);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideComponent);
+    };
+  }, [wrapper]);
+
+  const handleClickOutsideComponent = (e) => {
+    if (wrapper.current && !wrapper.current.contains(e.target)) {
+      setSuggestions([]);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    onSuggestionSelected(name, undefined);
+
+    if (query) {
+      getSearchResults(query).then((suggestions) => {
+        setSuggestions(suggestions);
+      });
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleClear = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSuggestionSelected(name, undefined);
+  };
+
+  const handleClick = (index: number) => {
+    const display = getDisplayValue(suggestions[index]);
+    const value = getFieldValue(suggestions[index]);
+    searchBox.current.value = display;
+    onSuggestionSelected(name, value);
+    setSuggestions([]);
+  };
+
+  return (
+    <div className={styles.autocomplete} ref={wrapper}>
+      <label className="cds--label">{labelText}</label>
+      <Layer className={classNames({ [styles.invalid]: invalid })}>
+        <Search
+          id="autosuggest"
+          onChange={handleChange}
+          onClear={handleClear}
+          ref={searchBox}
+          className={styles.autocompleteSearch}
+          {...searchProps}
+        />
+      </Layer>
+      {suggestions.length > 0 && (
+        <ul className={styles.suggestions}>
+          {suggestions.map((suggestion, index) => (
+            <li key={index} onClick={(e) => handleClick(index)} role="presentation">
+              {getDisplayValue(suggestion)}
+            </li>
+          ))}
+        </ul>
+      )}
+      {invalid ? <label className={classNames(styles.invalidMsg)}>{invalidText}</label> : <></>}
+    </div>
+  );
+};
--- packages/esm-billing-app/src/billing-form/billing-form.component.tsx
+++ packages/esm-billing-app/src/billing-form/billing-form.component.tsx
@@ -1,289 +1,205 @@
-import React, { useState } from 'react';
 import {
-  ButtonSet,
   Button,
-  RadioButtonGroup,
-  RadioButton,
-  Search,
+  ButtonSet,
+  Column,
+  Dropdown,
   Form,
+  Stack,
   Table,
-  TableHead,
   TableBody,
+  TableCell,
+  TableHead,
   TableHeader,
   TableRow,
-  TableCell,
-  InlineLoading,
 } from '@carbon/react';
-import styles from './billing-form.scss';
-import { useTranslation } from 'react-i18next';
-import { useFetchSearchResults, processBillItems } from '../billing.resource';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { getPatientUuidFromUrl } from '@openmrs/esm-patient-common-lib';
-import { showSnackbar } from '@openmrs/esm-framework';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+import { Autosuggest } from '../autosuggest/autosuggest.component';
+import { billingFormSchema, processBillItems } from '../billing.resource';
+import useBillableServices from '../hooks/useBillableServices';
+import { BillingService } from '../types';
+import styles from './billing-form.scss';
 import { mutate } from 'swr';
+import { showSnackbar } from '@openmrs/esm-framework';
 
 type BillingFormProps = {
   patientUuid: string;
   closeWorkspace: () => void;
 };
 
+type FormType = z.infer<typeof billingFormSchema>;
+
 const BillingForm: React.FC<BillingFormProps> = ({ closeWorkspace }) => {
   const { t } = useTranslation();
   const patientUuid = getPatientUuidFromUrl();
-  const [GrandTotal, setGrandTotal] = useState(0);
-  const [searchOptions, setsearchOptions] = useState([]);
+  const { billableServices, error, isLoading } = useBillableServices();
   const [BillItems, setBillItems] = useState([]);
   const [searchVal, setsearchVal] = useState('');
-  const [category, setCategory] = useState('');
 
-  const toggleSearch = (choiceSelected) => {
-    (document.getElementById('searchField') as HTMLInputElement).disabled = false;
-
-    if (choiceSelected == 'Stock Item') {
-      setCategory('Stock Item');
-    } else {
-      setCategory('Service');
-    }
-  };
-
-  const calculateTotal = (event, itemName) => {
-    const Qnty = event.target.value;
-    const price = (document.getElementById(event.target.id + 'Price') as HTMLInputElement).innerHTML;
-    const total = parseInt(price) * Qnty;
-    (document.getElementById(event.target.id + 'Total') as HTMLInputElement).innerHTML = total.toString();
-
-    const updateItem = BillItems.filter((o) => o.Item.toLowerCase().includes(itemName.toLowerCase()));
-
-    updateItem.map((o) => (o.Qnty = Qnty));
-    updateItem.map((o) => (o.Total = total));
-
-    const totals = Array.from(document.querySelectorAll('[id$="Total"]'));
-
-    let addUpTotals = 0;
-    totals.forEach((tot) => {
-      var getTot = (tot as HTMLInputElement).innerHTML;
-      addUpTotals += parseInt(getTot);
-    });
-    setGrandTotal(addUpTotals);
-  };
-
-  const CalculateTotalAfteraddBillItem = () => {
-    let sum = 0;
-    BillItems.map((o) => (sum += o.Price));
-
-    setGrandTotal(sum);
-  };
-
-  const addItemToBill = (event, itemid, itemname, itemcategory, itemPrice) => {
-    BillItems.push({
-      uuid: itemid,
-      Item: itemname,
-      Qnty: 1,
-      Price: itemPrice,
-      Total: itemPrice,
-      category: itemcategory,
-    });
-    setBillItems(BillItems);
-    setsearchOptions([]);
-    CalculateTotalAfteraddBillItem();
-    (document.getElementById('searchField') as HTMLInputElement).value = '';
-  };
-
-  //  filter items
-  const { data, error, isLoading, isValidating } = useFetchSearchResults(searchVal, category);
-
-  const filterItems = (val) => {
-    setsearchVal(val);
-
-    if (isLoading) {
-    } else {
-      if (typeof data !== 'undefined') {
-        //set to null then repopulate
-        while (searchOptions.length > 0) {
-          searchOptions.pop();
-        }
-
-        const res = data as { results: any[] };
-
-        res.results.map((o) => {
-          if (
-            o.commonName &&
-            (o.commonName != '' || o.commonName != null) &&
-            (o.purchasePrice != '' || o.purchasePrice != null)
-          ) {
-            searchOptions.push({
-              uuid: o.uuid,
-              Item: o.commonName,
-              Qnty: 1,
-              Price: o?.purchasePrice,
-              Total: o?.purchasePrice,
-              category: 'StockItem',
-            });
-          } else {
-            if (o.name.toLowerCase().includes(searchVal.toLowerCase())) {
-              searchOptions.push({
-                uuid: o.uuid,
-                Item: o.name,
-                Qnty: 1,
-                Price: o.servicePrices[0]?.price,
-                Total: o.servicePrices[0]?.price,
-                category: 'Service',
-              });
-            }
-          }
-          setsearchOptions(searchOptions);
-        });
-      }
-    }
-  };
-
-  const postBillItems = () => {
-    const bill = {
+  const form = useForm<FormType>({
+    resolver: zodResolver(billingFormSchema),
+    defaultValues: {
       cashPoint: '54065383-b4d4-42d2-af4d-d250a1fd2590',
       cashier: 'f9badd80-ab76-11e2-9e96-0800200c9a66',
-      lineItems: [],
-      payments: [],
       patient: patientUuid,
       status: 'PENDING',
-    };
+      lineItems: [],
+      payments: [],
+    },
+  });
 
-    BillItems.map((o) => {
-      if (o.category == 'StockItem') {
-        bill.lineItems.push({
-          item: o.uuid,
-          quantity: parseInt(o.Qnty),
-          price: o.Price,
-          priceName: 'Default',
-          priceUuid: '7b9171ac-d3c1-49b4-beff-c9902aee5245',
-          lineItemOrder: 0,
-          paymentStatus: 'PENDING',
-        });
-      } else {
-        bill.lineItems.push({
-          billableService: o.uuid,
-          quantity: parseInt(o.Qnty),
-          price: o.Price,
-          priceName: 'Default',
-          priceUuid: '7b9171ac-d3c1-49b4-beff-c9902aee5245',
-          lineItemOrder: 0,
-          paymentStatus: 'PENDING',
-        });
-      }
-    });
+  const onSubmit = async (values: FormType) => {
+    try {
+      await processBillItems(values);
+      closeWorkspace();
+      mutate((key) => typeof key === 'string' && key.startsWith(`/ws/rest/v1/cashier/bill`), undefined, {
+        revalidate: true,
+      });
+      showSnackbar({
+        title: t('billItems', 'Save Bill'),
+        subtitle: 'Bill processing has been successful',
+        kind: 'success',
+        timeoutInMs: 3000,
+      });
+    } catch (e) {
+      showSnackbar({ title: 'Bill processing error', kind: 'error', subtitle: e });
+    }
+  };
 
-    const url = `/ws/rest/v1/cashier/bill`;
-    processBillItems(bill).then(
-      (resp) => {
-        closeWorkspace();
-        mutate((key) => typeof key === 'string' && key.startsWith(url), undefined, { revalidate: true });
-        showSnackbar({
-          title: t('billItems', 'Save Bill'),
-          subtitle: 'Bill processing has been successful',
-          kind: 'success',
-          timeoutInMs: 3000,
-        });
-      },
-      (error) => {
-        showSnackbar({ title: 'Bill processing error', kind: 'error', subtitle: error });
-      },
+  const handleSearch = async (searchText: string) => {
+    setsearchVal(searchText);
+    return billableServices.filter((searvice) =>
+      searvice.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()),
     );
   };
 
+  const lineItemsOnservable = form.watch('lineItems');
+
   return (
-    <Form className={styles.form}>
-      <div className={styles.billingFormContainer}>
-        <div className={styles.contentWrapper}>
-          <RadioButtonGroup
-            legendText={t('selectCategory', 'Select category')}
-            name="radio-button-group"
-            defaultSelected="radio-1"
-            className={styles.billingItem}
-            onChange={toggleSearch}>
-            <RadioButton labelText={t('stockItem', 'Stock Item')} value="Stock Item" id="radio-1" />
-            <RadioButton labelText={t('service', 'Service')} value="Service" id="radio-2" />
-          </RadioButtonGroup>
-
-          <div>
-            <Search
-              id="searchField"
-              size="lg"
-              placeholder="Find your drugs here..."
-              labelText="Search"
-              closeButtonLabelText="Clear search input"
-              onChange={(e) => filterItems(e.target.value)}
-              className={styles.billingItem}
-            />
-
-            {isLoading && <InlineLoading status="active" iconDescription="Loading" description="Loading data..." />}
-
-            <ul className={styles.searchContent}>
-              {searchVal &&
-                searchOptions.map((row) => (
-                  <li className={`${styles.searchItem} ${parseInt(row.Price) === 0 && styles.searchDisable}`}>
-                    <Button
-                      disabled={row.Price == 0}
-                      id={row.uuid}
-                      onClick={(e) => addItemToBill(e, row.uuid, row.Item, row.category, row.Price)}
-                      style={{ background: 'inherit', color: 'black' }}>
-                      {row.Item} Qnty.{row.Qnty} Ksh.{row.Price}
-                    </Button>
-                  </li>
-                ))}
-            </ul>
-          </div>
-
-          <Table aria-label="sample table" className={styles.billingItem}>
+    <Form onSubmit={form.handleSubmit(onSubmit)}>
+      <Stack gap={4} className={styles.grid}>
+        <Column>
+          <Autosuggest
+            value={searchVal}
+            onClear={() => setsearchVal('')}
+            getDisplayValue={(item: BillingService) => item.name}
+            getFieldValue={(item: BillingService) => item.uuid}
+            getSearchResults={handleSearch}
+            onSuggestionSelected={(field, value) => {
+              if (value) {
+                form.setValue('lineItems', [
+                  ...lineItemsOnservable,
+                  {
+                    billableService: value,
+                    lineItemOrder: 0,
+                    quantity: 1,
+                    price: 0,
+                    paymentStatus: 'PENDING',
+                    priceName: 'Default',
+                  },
+                ]);
+              }
+              setsearchVal('');
+            }}
+            labelText={t('search', 'Search')}
+            placeholder={t('searchPlaceHolder', 'Find your billables here...')}
+          />
+        </Column>
+        <Column className={styles.billingItem}>
+          <Table aria-label="sample table">
             <TableHead>
               <TableRow>
                 <TableHeader>Item</TableHeader>
                 <TableHeader>Quantity</TableHeader>
+                <TableHeader>PaymentMethod</TableHeader>
                 <TableHeader>Price</TableHeader>
                 <TableHeader>Total</TableHeader>
               </TableRow>
             </TableHead>
             <TableBody>
-              {BillItems && Array.isArray(BillItems) ? (
-                BillItems.map((row) => (
+              {lineItemsOnservable.map(({ billableService, quantity, price }, index) => {
+                const service = billableServices.find((serv) => serv.uuid === billableService);
+                return (
                   <TableRow>
-                    <TableCell>{row.Item}</TableCell>
+                    <TableCell>{service.name}</TableCell>
                     <TableCell>
-                      <input
-                        type="number"
-                        className="form-control"
-                        id={row.Item}
-                        min={0}
-                        max={100}
-                        value={row.Qnty}
-                        onChange={(e) => {
-                          calculateTotal(e, row.Item);
-                          row.Qnty = e.target.value;
-                        }}
+                      <Controller
+                        control={form.control}
+                        name={`lineItems.${index}.quantity` as any}
+                        render={({ field }) => (
+                          <input
+                            ref={field.ref}
+                            value={field.value}
+                            onChange={({ target: { value } }) => {
+                              field.onChange(value);
+                            }}
+                            type="number"
+                            className="form-control"
+                            id={billableService}
+                            min={0}
+                            max={100}
+                          />
+                        )}
                       />
                     </TableCell>
-                    <TableCell id={row.Item + 'Price'}>{row.Price}</TableCell>
-                    <TableCell id={row.Item + 'Total'} className="totalValue">
-                      {row.Total}
+                    <TableCell>
+                      <Controller
+                        control={form.control}
+                        name={`lineItems.${index}.priceUuid`}
+                        render={({ field }) => (
+                          <Dropdown
+                            ref={field.ref}
+                            invalid={form.formState.errors[field.name]?.message}
+                            invalidText={form.formState.errors[field.name]?.message}
+                            id="priceUuid"
+                            onChange={(e) => {
+                              field.onChange(e.selectedItem);
+                              const price = service.servicePrices.find((p) => p.uuid === e.selectedItem)?.price;
+                              form.setValue(`lineItems.${index}.price`, price ?? 0);
+                            }}
+                            initialSelectedItem={field.value}
+                            label="Choose price"
+                            items={service.servicePrices.map((r) => r.uuid)}
+                            itemToString={(item) => service.servicePrices.find((r) => r.uuid === item)?.name ?? ''}
+                          />
+                        )}
+                      />
+                    </TableCell>
+                    <TableCell id={service.name + 'Price'}>{price}</TableCell>
+                    <TableCell id={service.name + 'Total'} className="totalValue">
+                      {price * quantity}
                     </TableCell>
                   </TableRow>
-                ))
-              ) : (
-                <p>Loading...</p>
-              )}
+                );
+              })}
               <TableRow>
                 <TableCell></TableCell>
                 <TableCell></TableCell>
+                <TableCell></TableCell>
+
                 <TableCell style={{ fontWeight: 'bold' }}>Grand Total:</TableCell>
-                <TableCell id="GrandTotalSum">{GrandTotal}</TableCell>
+                <TableCell id="GrandTotalSum">
+                  {lineItemsOnservable.reduce((prev, curr) => {
+                    const totoal = curr.quantity * curr.price;
+                    return prev + totoal;
+                  }, 0)}
+                </TableCell>
               </TableRow>
             </TableBody>
           </Table>
-        </div>
-      </div>
-      <ButtonSet className={styles.billingItem}>
+        </Column>
+      </Stack>
+
+      <ButtonSet className={styles.buttonSet}>
         <Button className={styles.button} kind="secondary" onClick={closeWorkspace}>
-          Discard
+          {t('discard', 'Discard')}
         </Button>
-        <Button className={styles.button} kind="primary" onClick={postBillItems}>
-          Save & Close
+        <Button className={styles.button} kind="primary" type="submit" disabled={form.formState.isSubmitting}>
+          {t('saveAndClose', 'Save & Close')}
         </Button>
       </ButtonSet>
     </Form>
