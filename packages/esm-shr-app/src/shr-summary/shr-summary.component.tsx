2561e32e6fa223e7eb77389abe0da994fe1ab244
its-kios09 <legacyitsolution@gmail.com>
2024-09-14 01:02:04 +0300

    (fix) importation

--- packages/esm-shr-app/src/shr-summary/shr-summary.component.tsx
+++ packages/esm-shr-app/src/shr-summary/shr-summary.component.tsx
@@ -3,10 +3,10 @@ import { CardHeader, EmptyDataIllustration, ErrorState, getPatientUuidFromUrl } 
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import usePatientIdentifiers from '../hooks/usePatientIdentifiers';
-import PatientSHRSummartTable from './tables/shr-summary-table.component';
 import styles from './tables/shr-tables.scss';
 import { useConfig } from '@openmrs/esm-framework';
 import { ReferralConfigObject } from '../config-schema';
+import PatientSHRSummaryTable from './tables/shr-summary-table.component';
 const SHRSummaryPanel = () => {
   const patientUuid = getPatientUuidFromUrl();
   const { t } = useTranslation();
