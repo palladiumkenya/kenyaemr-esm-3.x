d96686c9a783d9f81f0d4893cac98fd3f8d9cb81
Makombe Kennedy <kennedymakombe@gmail.com>
2024-10-01 09:34:23 +0300

    (fix): Show regimeline based on patient age when regimen was started (#368)

--- packages/esm-care-panel-app/src/regimen-editor/utils.tsx
+++ packages/esm-care-panel-app/src/regimen-editor/utils.tsx
@@ -22,22 +22,21 @@ export function filterRegimenData(regimenData: RegimenLineGroup[] | undefined, p
   return regimenData.filter((group) => group.regimenline.startsWith(filterCriterion));
 }
 
-export function calculateAge(birthDateString: string | null | undefined): number {
+export function calculateAge(birthDateString: string | null | undefined, visitDate: Date): number {
   if (!birthDateString) {
     return 0;
   }
 
-  const today = new Date();
   const birthDate = new Date(birthDateString);
 
   if (isNaN(birthDate.getTime())) {
     return 0;
   }
 
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const m = today.getMonth() - birthDate.getMonth();
+  let age = visitDate.getFullYear() - birthDate.getFullYear();
+  const m = visitDate.getMonth() - birthDate.getMonth();
 
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+  if (m < 0 || (m === 0 && visitDate.getDate() < birthDate.getDate())) {
     age--;
   }
 
