736ae92cfa35304a78bb181b39dfce04520114e9
its-kios09 <legacyitsolution@gmail.com>
2024-10-09 07:01:58 +0300

    (refactor) added an endpoint that pulls patients who are marked as deceased

--- packages/esm-morgue-app/src/card/row-elements/unit-patient-name.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../unit-patient-card.scss'; // Updated the path
-
-const UnitPatientName: React.FC<{ patient: any }> = ({ patient }) => {
-  return <div className={styles.unitPatientName}>{patient?.person?.preferredName?.display}</div>;
-};
-
-export default UnitPatientName;
