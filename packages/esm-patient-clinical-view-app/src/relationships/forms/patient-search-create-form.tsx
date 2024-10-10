910bbc6e727ae0c738a771ed3dc5e0679b56f0db
Laurent Ouma <lawiomosh3@gmail.com>
2024-10-10 13:52:28 +0300

    Patiend creation fields  added

--- packages/esm-patient-clinical-view-app/src/relationships/forms/patient-search-create-form.tsx
+++ packages/esm-patient-clinical-view-app/src/relationships/forms/patient-search-create-form.tsx
@@ -1,14 +1,36 @@
-import { Column } from '@carbon/react';
-import React, { useState } from 'react';
+import {
+  Button,
+  Column,
+  DatePicker,
+  DatePickerInput,
+  Dropdown,
+  RadioButton,
+  RadioButtonGroup,
+  TextInput,
+} from '@carbon/react';
+import { Calculator } from '@carbon/react/icons';
+import { showModal } from '@openmrs/esm-framework';
+import React, { useMemo, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { Autosuggest } from '../../autosuggest/autosuggest.component';
 import SearchEmptyState from '../../autosuggest/search-empty-state.component';
+import { contactListConceptMap } from '../../contact-list/contact-list-concept-map';
 import { fetchPerson } from '../relationship.resources';
 import styles from './form.scss';
 
 const PatientSearchCreate = () => {
-  const form = useFormContext<{ personB: string }>();
+  const form = useFormContext<{
+    personB: string;
+    givenName: string;
+    familyName: string;
+    middleName: string;
+    gender: 'M' | 'F';
+    birthdate: Date;
+    maritalStatus: string;
+    address: string;
+    phoneNumber: string;
+  }>();
   const { t } = useTranslation();
   const [mode, setMode] = useState<'search' | 'create'>('search');
   const searchPatient = async (query: string) => {
@@ -17,6 +39,21 @@ const PatientSearchCreate = () => {
   };
 
   const handleAdd = () => setMode('create');
+  const maritalStatus = useMemo(
+    () =>
+      Object.entries(contactListConceptMap['1056AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'].answers).map(([uuid, display]) => ({
+        label: display,
+        value: uuid,
+      })),
+    [],
+  );
+
+  const handleCalculateBirthDate = () => {
+    const dispose = showModal('birth-date-calculator', {
+      onClose: () => dispose(),
+      props: { date: new Date(), onBirthDateChange: (date) => form.setValue('birthdate', date) },
+    });
+  };
 
   return (
     <>
@@ -55,7 +92,143 @@ const PatientSearchCreate = () => {
       )}
       {mode === 'create' && (
         <>
-          <Column></Column>
+          <span className={styles.sectionHeader}>{t('demographics', 'Demographics')}</span>
+          <Column>
+            <Controller
+              control={form.control}
+              name="givenName"
+              render={({ field }) => (
+                <TextInput
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  {...field}
+                  placeholder="First name"
+                  labelText={t('firstName', 'First name')}
+                />
+              )}
+            />
+          </Column>
+          <Column>
+            <Controller
+              control={form.control}
+              name="middleName"
+              render={({ field }) => (
+                <TextInput
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  {...field}
+                  placeholder="Middle name"
+                  labelText={t('middleName', 'Middle name')}
+                />
+              )}
+            />
+          </Column>
+          <Column>
+            <Controller
+              control={form.control}
+              name="familyName"
+              render={({ field }) => (
+                <TextInput
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  {...field}
+                  placeholder="Last name"
+                  labelText={t('lastName', 'Last name')}
+                />
+              )}
+            />
+          </Column>
+          <Column>
+            <Controller
+              control={form.control}
+              name="gender"
+              render={({ field }) => (
+                <RadioButtonGroup
+                  legendText={t('sex', 'Sex')}
+                  {...field}
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  className={styles.billingItem}>
+                  <RadioButton labelText={t('male', 'Male')} value="M" id="M" />
+                  <RadioButton labelText={t('female', 'Female')} value="F" id="F" />
+                </RadioButtonGroup>
+              )}
+            />
+          </Column>
+          <Column className={styles.facilityColumn}>
+            <Controller
+              control={form.control}
+              name="birthdate"
+              render={({ field }) => (
+                <DatePicker datePickerType="single" {...field}>
+                  <DatePickerInput
+                    invalid={form.formState.errors[field.name]?.message}
+                    invalidText={form.formState.errors[field.name]?.message}
+                    placeholder="mm/dd/yyyy"
+                    labelText={t('dateOfBirth', 'Date of birth')}
+                    size="xl"
+                    className={styles.datePickerInput}
+                  />
+                </DatePicker>
+              )}
+            />
+            <Button kind="ghost" renderIcon={Calculator} onClick={handleCalculateBirthDate}>
+              {t('fromAge', 'From Age')}
+            </Button>
+          </Column>
+          <Column>
+            <Controller
+              control={form.control}
+              name="maritalStatus"
+              render={({ field }) => (
+                <Dropdown
+                  ref={field.ref}
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  id="maritalStatus"
+                  titleText={t('maritalStatus', 'Marital status')}
+                  onChange={(e) => {
+                    field.onChange(e.selectedItem);
+                  }}
+                  initialSelectedItem={field.value}
+                  label="Choose option"
+                  items={maritalStatus.map((r) => r.value)}
+                  itemToString={(item) => maritalStatus.find((r) => r.value === item)?.label ?? ''}
+                />
+              )}
+            />
+          </Column>
+          <span className={styles.sectionHeader}>{t('contact', 'Contact')}</span>
+          <Column>
+            <Controller
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <TextInput
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  {...field}
+                  placeholder="Physical Address/Landmark"
+                  labelText={t('address', 'Address')}
+                />
+              )}
+            />
+          </Column>
+          <Column>
+            <Controller
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <TextInput
+                  {...field}
+                  invalid={form.formState.errors[field.name]?.message}
+                  invalidText={form.formState.errors[field.name]?.message}
+                  placeholder="Phone number"
+                  labelText={t('phoneNumber', 'Phone number')}
+                />
+              )}
+            />
+          </Column>
         </>
       )}
     </>
