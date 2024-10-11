a9c335b196979a5f028103e6e2e6fa78ba9ef0b4
its-kios09 <legacyitsolution@gmail.com>
2024-10-11 12:38:27 +0300

    (feat) Add bill table and workspace

--- packages/esm-morgue-app/src/panels/billing-history.component.tsx
+++ packages/esm-morgue-app/src/panels/billing-history.component.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ExtensionSlot, navigate, usePatient } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
-import { EmptyState } from '@openmrs/esm-patient-common-lib';
+import { EmptyState, getPatientUuidFromUrl } from '@openmrs/esm-patient-common-lib';
 
 const BillingHistoryView: React.FC = () => {
   const { t } = useTranslation();
-
+  const patientUuid = getPatientUuidFromUrl();
   return (
     <div>
-      <EmptyState displayText={'Billing history'} headerTitle={t('billingHistory', 'Billing history')} />
+      <ExtensionSlot name="patient-chart-billing-dashboard-slot" state={{ patientUuid }} />
     </div>
   );
 };
