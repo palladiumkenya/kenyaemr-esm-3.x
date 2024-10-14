d26cc9f3b1add2351073d0338966119407dd8eea
Fredrick Kioko <67967749+its-kios09@users.noreply.github.com>
2024-10-14 08:57:30 +0300

    (feat): Added bill history and workspace feature to Morgue Module (#397)
    * (feat) Add bill table and workspace
    
    * (refactor) Updated the json file
    
    * (feat) added styling to the table

--- packages/esm-morgue-app/src/panels/billing-history.component.tsx
+++ packages/esm-morgue-app/src/panels/billing-history.component.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ExtensionSlot, navigate, usePatient } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
-import { EmptyState } from '@openmrs/esm-patient-common-lib';
+import { EmptyState, getPatientUuidFromUrl } from '@openmrs/esm-patient-common-lib';
+import styles from './panels.scss';
 
 const BillingHistoryView: React.FC = () => {
   const { t } = useTranslation();
-
+  const patientUuid = getPatientUuidFromUrl();
   return (
-    <div>
-      <EmptyState displayText={'Billing history'} headerTitle={t('billingHistory', 'Billing history')} />
+    <div className={styles.table}>
+      <ExtensionSlot name="patient-chart-billing-dashboard-slot" state={{ patientUuid }} />
     </div>
   );
 };
