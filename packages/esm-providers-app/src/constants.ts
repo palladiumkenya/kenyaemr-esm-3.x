88bb2c70f9e9a968129445bbe9150c16df99e6b7
Laurent Ouma <98098891+Omoshlawi@users.noreply.github.com>
2024-10-23 10:08:23 +0300

    (fix) KHP3-6911: Fix provider app bugs - gender field, HWR credentials, and identifier type issues (#410)
    * KHP3-6913-fix-gender-field-in-provider-form-when-updating-not-picking-current-providers-gender
    
    * KHP3-6912-fix-auto-logout-of-user-when-credentials-to-hwr-are-invalid-or-not-provided
    
    * fixed equality operator
    
    * Haddled loading state of identifier type which prevented loading of default identifier type.Also fixed defaul identifier find function which used display instead of display
    
    * Cleaned code, removing magic values

--- packages/esm-providers-app/src/constants.ts
+++ packages/esm-providers-app/src/constants.ts
@@ -1,3 +1,21 @@
 export const moduleName = '@kenyaemr/esm-providers-app';
 export const providerBasePath = `${window.spaBase}/home/providers`;
 export const defaultPageSize = 5;
+/**
+ * Represents the error response from the Health Care Worker Registry API
+ * when no valid credentials are provided or found.
+ * @constant {string}
+ */
+export const HWR_API_NO_CREDENTIALS = 'NO_API_CREDENNTIALS';
+/**
+ * Represents the error response from the Health Care Worker Registry API
+ * when when resource is not found
+ * @constant {string}
+ */
+export const NOT_FOUND = 'NOT_FOUND';
+/**
+ * Represents the error response from the Health Care Worker Registry API
+ * When Uknown error occures
+ * @constant {string}
+ */
+export const UNKNOWN = 'UNKNOWN';
