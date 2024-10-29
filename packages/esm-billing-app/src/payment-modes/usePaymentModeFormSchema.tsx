d8ccbe805ea5b9e61b7761c163f6de40d13d9ad3
Donald Kibet <chelashawdonald@yahoo.com>
2024-10-29 17:51:18 +0300

    (feat) KHP3-6963 : Add ability to add and edit full payment mode attributes

---
+++ packages/esm-billing-app/src/payment-modes/usePaymentModeFormSchema.tsx
@@ -0,0 +1,52 @@
+import { z } from 'zod';
+
+const usePaymentModeFormSchema = () => {
+  const attributeTypeSchema = z
+    .object({
+      name: z.string(),
+      description: z.string(),
+      retired: z.boolean().default(false),
+      retiredReason: z.string().optional(),
+      format: z.string().optional(),
+      regExp: z.string().optional(),
+      required: z.boolean().default(false),
+    })
+    .refine(
+      (data) => {
+        if (data.retired === true) {
+          return !!data.retiredReason && data.retiredReason.trim().length > 0;
+        }
+        return true;
+      },
+      {
+        message: 'Retired reason is required when attribute type is retired',
+        path: ['retiredReason'],
+      },
+    );
+
+  const paymentModeFormSchema = z.object({
+    name: z
+      .string({
+        required_error: 'Payment mode name is required',
+        invalid_type_error: 'Payment mode name is required',
+      })
+      .min(1, 'Payment mode name is required'),
+    description: z
+      .string({
+        required_error: 'Payment mode description is required',
+        invalid_type_error: 'Payment mode description is required',
+      })
+      .min(1, 'Payment mode description is required'),
+    retired: z
+      .boolean({
+        invalid_type_error: 'Retired must be a boolean',
+      })
+      .optional()
+      .default(false), // Provides a default value if retired is not specified
+    attributeTypes: z.array(attributeTypeSchema).optional(),
+  });
+
+  return { paymentModeFormSchema };
+};
+
+export default usePaymentModeFormSchema;
