634b8420fc19487a84ea0998ad84a70223195b1b
Laurent Ouma <lawiomosh3@gmail.com>
2024-12-03 21:25:51 +0300

    mocked fetch of interventions and packages from api

--- packages/esm-billing-app/src/hooks/useInterventions.ts
+++ packages/esm-billing-app/src/hooks/useInterventions.ts
@@ -2,15 +2,21 @@ import { FetchResponse, openmrsFetch, useConfig } from '@openmrs/esm-framework';
 import useSWR from 'swr';
 import { BillingConfig } from '../config-schema';
 import { SHAIntervension } from '../types';
+import { interventions } from './benefits.mock';
 
 export const useInterventions = (code: string) => {
   const { hieBaseUrl } = useConfig<BillingConfig>();
 
   const url = `${hieBaseUrl}/master/benefit-master/code?searchKey=${code}`;
-  const { isLoading, error, data } = useSWR<FetchResponse<Array<SHAIntervension>>>(url, openmrsFetch);
+  const { isLoading, error, data } = useSWR<Array<SHAIntervension>>(url, async (key) => {
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 2000);
+    });
+    return interventions?.filter((inter) => inter.interventionCode.includes(code));
+  });
   return {
     isLoading,
-    interventions: data?.data ?? [],
+    interventions: data ?? [],
     error,
   };
 };
