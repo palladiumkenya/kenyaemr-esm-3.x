01285f159f33ebc6f66aa156ec56ab85326668e4
Donald Kibet <chelashawdonald@yahoo.com>
2024-12-10 12:04:01 +0300

    (feat) : Update dispensing to prevent processing of drugs with unsettled bill (#508)

--- packages/esm-billing-app/src/billable-services/billiable-item/test-order/test-order-action.component.tsx
+++ packages/esm-billing-app/src/billable-services/billiable-item/test-order/test-order-action.component.tsx
@@ -3,12 +3,13 @@ import { Order } from '@openmrs/esm-patient-common-lib';
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTestOrderBillStatus } from './test-order-action.resource';
-import { showModal } from '@openmrs/esm-framework';
+import { launchWorkspace, showModal } from '@openmrs/esm-framework';
 import { mutate } from 'swr';
 import styles from './test-order-action.scss';
+import { createMedicationDispenseProps } from './dispense.resource';
 
 type TestOrderProps = {
-  order: Order;
+  order?: Order;
   modalName?: string;
   additionalProps?: Record<string, unknown>;
   actionText?: string;
@@ -16,43 +17,25 @@ type TestOrderProps = {
     request: fhir.MedicationRequest;
   };
   handleOpenDispenseForm?: () => void;
+  closeable?: boolean;
 };
 
 enum FulfillerStatus {
   IN_PROGRESS = 'IN_PROGRESS',
 }
 
-// Memoize the comparison function
-const areEqual = (prevProps: TestOrderProps, nextProps: TestOrderProps) => {
-  if (prevProps?.order?.uuid !== nextProps?.order?.uuid) {
-    return false;
-  }
-  if (prevProps?.modalName !== nextProps?.modalName) {
-    return false;
-  }
-  if (prevProps?.actionText !== nextProps?.actionText) {
-    return false;
-  }
-  if (prevProps?.medicationRequestBundle?.request?.id !== nextProps?.medicationRequestBundle?.request?.id) {
-    return false;
-  }
-
-  // Only stringify additionalProps if everything else matches
-  return JSON.stringify(prevProps?.additionalProps) === JSON.stringify(nextProps?.additionalProps);
-};
-
-// Show the test order if the following conditions are met:
-// 1. The current visit is in-patient
-// 2. The test order has been paid in full
-// 3. The patient is an emergency patient
-const TestOrderAction: React.FC<TestOrderProps> = React.memo((props) => {
+const TestOrderAction: React.FC<TestOrderProps> = (props) => {
   const { order, modalName, additionalProps, actionText, medicationRequestBundle } = props;
   const { t } = useTranslation();
 
+  const isDispenseOrder = Object.hasOwn(props, 'medicationRequestBundle');
+  const dispenseFormProps = isDispenseOrder ? createMedicationDispenseProps(props) : null;
   const orderUuid = order?.uuid ?? medicationRequestBundle?.request?.id;
   const patientUuid = order?.patient?.uuid ?? medicationRequestBundle?.request?.subject?.reference?.split('/')[1];
+
   const { isLoading, hasPendingPayment } = useTestOrderBillStatus(orderUuid, patientUuid);
 
+  // Handle modal close and revalidation
   const handleModalClose = useCallback(() => {
     mutate((key) => typeof key === 'string' && key.startsWith(additionalProps?.mutateUrl as string), undefined, {
       revalidate: true,
@@ -60,9 +43,8 @@ const TestOrderAction: React.FC<TestOrderProps> = React.memo((props) => {
   }, [additionalProps?.mutateUrl]);
 
   const launchModal = useCallback(() => {
-    // TODO: Remove this once we have dispensing form using workspaces
-    if (props.handleOpenDispenseForm) {
-      props.handleOpenDispenseForm();
+    if (isDispenseOrder) {
+      launchWorkspace('dispense-workspace', dispenseFormProps);
       return;
     }
 
@@ -84,21 +66,25 @@ const TestOrderAction: React.FC<TestOrderProps> = React.memo((props) => {
     );
   }
 
-  if (order?.fulfillerStatus === FulfillerStatus.IN_PROGRESS) {
+  if (order?.fulfillerStatus === FulfillerStatus.IN_PROGRESS || (isDispenseOrder && !props?.closeable)) {
     return null;
   }
 
   const buttonText = hasPendingPayment
     ? t('unsettledBill', 'Unsettled bill')
-    : Object.hasOwn(props, 'medicationRequestBundle')
+    : isDispenseOrder
     ? actionText ?? t('dispense', 'Dispense')
     : actionText ?? t('pickLabRequest', 'Pick Lab Request');
 
   return (
-    <Button kind="primary" className={styles.actionButton} disabled={hasPendingPayment} onClick={launchModal}>
+    <Button
+      kind="primary"
+      className={!isDispenseOrder ? styles.actionButton : ''}
+      disabled={hasPendingPayment}
+      onClick={launchModal}>
       {buttonText}
     </Button>
   );
-}, areEqual);
+};
 
 export default TestOrderAction;
