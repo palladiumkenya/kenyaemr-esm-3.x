9793ddbd6ceb6d18707cc9c2ef53a2660d717559
Donald Kibet <chelashawdonald@yahoo.com>
2024-12-19 08:58:39 +0300

    wip cross border

--- packages/esm-cross-border-app/src/index.ts
+++ packages/esm-cross-border-app/src/index.ts
@@ -1,6 +1,9 @@
 import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
 import CrossBorderApp from './root.component';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
+import SideMenu from './components/side-menu/side-menu.component';
+import MPISearch from './components/search/mpi-search.component';
 
 const moduleName = '@kenyaemr/esm-cross-border-app';
 
@@ -12,6 +15,16 @@ const options = {
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 export const crossBorderApp = getSyncLifecycle(CrossBorderApp, options);
+
+export const crossBorderSideNav = getSyncLifecycle(SideMenu, options);
+
+export const crossBorderSearch = getSyncLifecycle(MPISearch, options);
+
+// Dashboard link for the search page
+export const crossBorderSearchDashboardLink = getSyncLifecycle(
+  createDashboardLink({ moduleName, path: 'search', title: 'Search' }),
+  options,
+);
 
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
--- packages/esm-cross-border-app/src/root.component.tsx
+++ packages/esm-cross-border-app/src/root.component.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import { setLeftNav, unsetLeftNav } from '@openmrs/esm-framework';
+import Dashboard from './components/dashboard/home-dashboard.component';
 
 const CrossBorderApp = () => {
-  return <div>Cross Border App</div>;
+  const spaBasePath = `${window.spaBase}/cross-border`;
+
+  useEffect(() => {
+    setLeftNav({ name: 'cross-border-page-dashboard-slot', basePath: spaBasePath });
+    return () => unsetLeftNav('cross-border-page-dashboard-slot');
+  }, [spaBasePath]);
+
+  return (
+    <main>
+      <BrowserRouter basename={window.spaBase}>
+        <Routes>
+          <Route path="/cross-border" element={<Dashboard />} />
+          <Route path="/cross-border/:dashboard/*" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 };
 
 export default CrossBorderApp;
