15543464a5e63d528469e386a3c1b00a89a0acf9
its-kios09 <legacyitsolution@gmail.com>
2025-01-08 23:53:37 +0300

    Revert "(feat) added end relationship functionality"
    This reverts commit e457ddd3e594d4dcb86f0526803d0f610bba5730.

--- packages/esm-patient-clinical-view-app/src/case-management/table/case-management-list-inactive-component.tsx
+++ packages/esm-patient-clinical-view-app/src/case-management/table/case-management-list-inactive-component.tsx
@@ -13,7 +13,7 @@ import {
   Search,
   Layer,
   Tile,
-  Tag,
+  OverflowMenu,
 } from '@carbon/react';
 import { CardHeader, EmptyDataIllustration } from '@openmrs/esm-patient-common-lib';
 import { ConfigurableLink, isDesktop, useLayoutType, useSession } from '@openmrs/esm-framework';
@@ -43,7 +43,6 @@ const CaseManagementListInActive: React.FC<CaseManagementListInActiveProps> = ({
     { key: 'names', header: t('names', 'Names') },
     { key: 'dateofstart', header: t('dateofstart', 'Start Date') },
     { key: 'dateofend', header: t('dateofend', 'End Date') },
-    { key: 'status', header: t('status', 'Status') },
   ];
 
   const filteredCases = inactiveCasesData?.data.results.filter(
@@ -53,24 +52,21 @@ const CaseManagementListInActive: React.FC<CaseManagementListInActiveProps> = ({
         caseData.personB.display.toLowerCase().includes(searchTerm.toLowerCase())),
   );
 
-  const tableRows = filteredCases?.slice((currentPage - 1) * pageSize, currentPage * pageSize).map((caseData) => ({
-    id: caseData.uuid,
-    names: (
-      <ConfigurableLink
-        style={{ textDecoration: 'none', maxWidth: '50%' }}
-        to={patientChartUrl}
-        templateParams={{ patientUuid: caseData.personB.uuid }}>
-        {uppercaseText(extractNameString(caseData.personB.display))}
-      </ConfigurableLink>
-    ),
-    dateofstart: new Date(caseData.startDate).toLocaleDateString(),
-    dateofend: new Date(caseData.endDate).toLocaleDateString(),
-    status: caseData.endDate ? (
-      <Tag type="red" size="lg">
-        {t('discontinued', 'Discontinued')}
-      </Tag>
-    ) : null,
-  }));
+  const tableRows = filteredCases
+    ?.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+    .map((caseData, index) => ({
+      id: caseData.uuid,
+      names: (
+        <ConfigurableLink
+          style={{ textDecoration: 'none', maxWidth: '50%' }}
+          to={patientChartUrl}
+          templateParams={{ patientUuid: caseData.personB.uuid }}>
+          {uppercaseText(extractNameString(caseData.personB.display))}
+        </ConfigurableLink>
+      ),
+      dateofstart: new Date(caseData.startDate).toLocaleDateString(),
+      dateofend: new Date(caseData.endDate).toLocaleDateString(),
+    }));
 
   useEffect(() => {
     const count = filteredCases?.length || 0;
