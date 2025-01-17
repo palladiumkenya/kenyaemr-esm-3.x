f11a2c56575f8c0cf06e8020cbfa682e285de1a2
Donald Kibet <chelashawdonald@yahoo.com>
2025-01-17 14:36:28 +0300

    (feat) : Add ability to register patient from HIE

--- packages/esm-shr-app/src/shr-summary/shr-summary.component.tsx
+++ packages/esm-shr-app/src/shr-summary/shr-summary.component.tsx
@@ -1,4 +1,4 @@
-import { DataTableSkeleton, Layer, Tile } from '@carbon/react';
+import { DataTableSkeleton, Layer, Tile, Tabs, TabList, Tab, TabPanel, TabPanels } from '@carbon/react';
 import { CardHeader, EmptyDataIllustration, ErrorState, getPatientUuidFromUrl } from '@openmrs/esm-patient-common-lib';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -7,8 +7,9 @@ import styles from './tables/shr-tables.scss';
 import { useConfig } from '@openmrs/esm-framework';
 import { ReferralConfigObject } from '../config-schema';
 import PatientSHRSummaryTable from './tables/shr-summary-table.component';
-const SHRSummaryPanel = () => {
-  const patientUuid = getPatientUuidFromUrl();
+import DependentsComponent from '../referrals/dependents/dependents.component';
+
+const SHRSummaryPanel = ({ patient, patientUuid }) => {
   const { t } = useTranslation();
   const { socialHealthAuthorityIdentifierType } = useConfig<ReferralConfigObject>();
   const { error, isLoading, hasType } = usePatientIdentifiers(patientUuid);
@@ -21,28 +22,41 @@ const SHRSummaryPanel = () => {
     return <ErrorState error={error} headerTitle={t('shrSummary', 'SHR Summary')} />;
   }
 
-  if (!hasType(socialHealthAuthorityIdentifierType)) {
-    return (
-      <div>
-        <Layer>
-          <Tile className={styles.tile}>
-            <CardHeader title={t('shrRecords', 'SHR Records')}>{''}</CardHeader>
-            <EmptyDataIllustration />
-            <p className={styles.content}>
-              {t(
-                'shrError',
-                'Patient must have CR Number.Please register the patient with the client registry to retrieve their SHR Records',
-              )}
-            </p>
-          </Tile>
-        </Layer>
-      </div>
-    );
-  }
+  // if (!hasType(socialHealthAuthorityIdentifierType)) {
+  //   return (
+  //     <div>
+  //       <Layer>
+  //         <Tile className={styles.tile}>
+  //           <CardHeader title={t('shrRecords', 'SHR Records')}>{''}</CardHeader>
+  //           <EmptyDataIllustration />
+  //           <p className={styles.content}>
+  //             {t(
+  //               'shrError',
+  //               'Patient must have CR Number.Please register the patient with the client registry to retrieve their SHR Records',
+  //             )}
+  //           </p>
+  //         </Tile>
+  //       </Layer>
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div className={`omrs-main-content`}>
-      <PatientSHRSummaryTable />
+    <div>
+      <Tabs>
+        <TabList aria-label="List of tabs" contained>
+          <Tab>{t('dependents', 'Dependents')}</Tab>
+          <Tab>{t('pullSHRRecords', 'Pull SHR Records')}</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <DependentsComponent patientUuid={patientUuid} patient={patient} />
+          </TabPanel>
+          <TabPanel>
+            <PatientSHRSummaryTable />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 };
