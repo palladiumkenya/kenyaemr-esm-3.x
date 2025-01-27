6968ebb92d8cf264d4f780e504432425b6f261e4
Laurent Ouma <lawiomosh3@gmail.com>
2025-01-27 16:10:05 +0300

    Using json payload file as source for intervensions

--- packages/esm-billing-app/src/hooks/useInterventions.ts
+++ packages/esm-billing-app/src/hooks/useInterventions.ts
@@ -22,6 +22,23 @@ export const toQueryParams = (q: Record<string, any>) => {
   );
 };
 
+type Data = {
+  status: string;
+  data: Array<{
+    interventionName: string;
+    interventionCode: string;
+    interventionPackage: string;
+    interventionSubPackage: string;
+    interventionDescription?: string;
+    insuranceSchemes: Array<{
+      rules: Array<{
+        ruleName: string;
+        ruleCode: string;
+        value: string;
+      }>;
+    }>;
+  }>;
+};
 export const useInterventions = (filters: InterventionsFilter) => {
   const fetcher = (url: string) => {
     return openmrsFetch(url, {
@@ -36,29 +53,33 @@ export const useInterventions = (filters: InterventionsFilter) => {
     });
   };
 
-  const url = `${restBaseUrl}/insuranceclaims/claims/interventions/query${toQueryParams(filters)}`;
-  const { isLoading, error, data } = useSWR<
-    FetchResponse<{
-      status: string;
-      data: Array<{
-        interventionName: string;
-        interventionCode: string;
-        interventionPackage: string;
-        interventionSubPackage: string;
-        interventionDescription?: string;
-      }>;
-    }>
-  >(url, fetcher);
+  const url = `${restBaseUrl}/kenyaemr/sha-interventions${toQueryParams({
+    ...filters,
+    synchronize: false,
+  })}`;
+  const { isLoading, error, data } = useSWR<{ data: Data }>(url, async (url: string) => {
+    const payload = require('./payload.json');
+    return { data: payload };
+  });
+
   return {
     isLoading,
-    interventions: (data?.data?.data ?? []).map(
-      ({ interventionCode, interventionName, interventionPackage, interventionSubPackage, interventionDescription }) =>
+    interventions: (data?.data as Data | undefined)?.data
+      ?.filter((d) => d.interventionPackage === filters.package_code)
+      ?.map(
         ({
           interventionCode,
-          subCategoryBenefitsPackage: interventionSubPackage,
           interventionName,
-        } as SHAIntervention),
-    ),
+          interventionPackage,
+          interventionSubPackage,
+          interventionDescription,
+        }) =>
+          ({
+            interventionCode,
+            subCategoryBenefitsPackage: interventionSubPackage,
+            interventionName,
+          } as SHAIntervention),
+      ),
     error,
   };
 };
