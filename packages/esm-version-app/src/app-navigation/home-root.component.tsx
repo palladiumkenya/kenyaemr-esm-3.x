c6c3f9494e03421b9fbe42c523aeacb1c0336939
Laurent Ouma <98098891+Omoshlawi@users.noreply.github.com>
2025-01-28 11:08:32 +0400

    KHP3-7349-Implement Side navs and Nav groups with icons (#564)
    * Grouped side nav link and implemented nav-util components and functions.
    
    * fix: update import statement for CarbonIconType to use type syntax
    
    * fix: add order property to patientChartClinicalConsultationNavGroup in routes.json

---
+++ packages/esm-version-app/src/app-navigation/home-root.component.tsx
@@ -0,0 +1,34 @@
+import { ExtensionSlot, useExtensionStore } from '@openmrs/esm-framework';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const HomeRoot = () => {
+  const baseName = window.getOpenmrsSpaBase() + 'home';
+
+  return (
+    <BrowserRouter basename={baseName}>
+      <Routes>
+        <Route path="/" element={<ExtensionSlot name="home-dashboard-slot" />} />
+        <Route path="/providers/*" element={<ExtensionSlot name="providers-dashboard-slot" />} />
+        <Route path="/referrals/*" element={<ExtensionSlot name="referrals-slot" />} />
+        <Route path="/bed-admission/*" element={<ExtensionSlot name="bed-admission-dashboard-slot" />} />
+        {/* Patient services Routes */}
+        <Route path="/appointments/*" element={<ExtensionSlot name="clinical-appointments-dashboard-slot" />} />
+        <Route path="/service-queues/*" element={<ExtensionSlot name="service-queues-dashboard-slot" />} />
+        {/* Diagnostics routes */}
+        <Route path="/lab-manifest/*" element={<ExtensionSlot name="lab-manifest-slot" />} />
+        <Route path="/laboratory/*" element={<ExtensionSlot name="laboratory-dashboard-slot" />} />
+        <Route path="/procedure/*" element={<ExtensionSlot name="procedure-dashboard-slot" />} />
+        <Route path="/imaging-orders/*" element={<ExtensionSlot name="imaging-dashboard-slot" />} />
+        {/* lINKAGE services Routes */}
+        <Route path="/pharmacy/*" element={<ExtensionSlot name="pharmacy-dashboard-slot" />} />
+        <Route path="/case-management/*" element={<ExtensionSlot name="case-management-dashboard-slot" />} />
+        <Route path="/peer-calendar/*" element={<ExtensionSlot name="peer-calendar-dashboard-slot" />} />
+        {/* Billing routes */}
+        <Route path="/billing/*" element={<ExtensionSlot name="billing-dashboard-slot" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default HomeRoot;
