024952d1f95abaff4de77c21c920aa04b5fd6d58
Laurent Ouma <lawiomosh3@gmail.com>
2025-02-19 14:52:16 +0300

    feat: update FacilitySetup header to use translation for facility details

--- packages/esm-admin-app/src/components/facility-setup/facility-setup.component.tsx
+++ packages/esm-admin-app/src/components/facility-setup/facility-setup.component.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import { Layer } from '@carbon/react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Layer, Button, MenuItem, InlineLoading } from '@carbon/react';
-import styles from './facility-setup.scss';
 import FacilityInfo from './facility-info.component';
+import styles from './facility-setup.scss';
 import Header from './header/header.component';
-import { showSnackbar } from '@openmrs/esm-framework';
 
 const FacilitySetup: React.FC = () => {
   const { t } = useTranslation();
   return (
     <div className="omrs-main-content">
-      <Header title="Facility Details" />
+      <Header title={t('facilityDetails', 'Facility Details')} />
 
       <Layer className={styles.tableLayer}>
         <FacilityInfo />
