91d7bc6ae7b396ccab955bd7421b45a2594fe323
Ogollah <kogollah2011@gmail.com>
2025-02-25 17:21:17 +0300

    add comboBox for role scope select

--- packages/esm-admin-app/src/components/users/userManagementFormSchema.tsx
+++ packages/esm-admin-app/src/components/users/userManagementFormSchema.tsx
@@ -64,6 +64,7 @@ const UserManagementFormSchema = () => {
         activeFrom: z.date().optional(),
       })
       .optional(),
+    stockRole: z.string().optional(),
   });
 
   return { userManagementFormSchema };
