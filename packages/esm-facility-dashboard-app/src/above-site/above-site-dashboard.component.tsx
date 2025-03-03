259baa71c698812d3aa6b35555c18735f9542078
Laurent Ouma <lawiomosh3@gmail.com>
2025-03-03 15:00:32 +0300

    feat: enhance Above Site Dashboard with Superset integration and styling

--- packages/esm-facility-dashboard-app/src/above-site/above-site-dashboard.component.tsx
+++ packages/esm-facility-dashboard-app/src/above-site/above-site-dashboard.component.tsx
@@ -1,13 +1,31 @@
+import { Button, Layer } from '@carbon/react';
+import { useConfig } from '@openmrs/esm-framework';
 import React from 'react';
-import FacilityDashboardHeader from '../components/header/header.component';
 import { useTranslation } from 'react-i18next';
+import FacilityDashboardHeader from '../components/header/header.component';
+import { FacilityDashboardConfigObject } from '../config-schema';
+import styles from './above-site.scss';
 
 const AboveSiteDashboard = () => {
   const { t } = useTranslation();
+  const { facilityDashboardAboveSiteUrl } = useConfig<FacilityDashboardConfigObject>();
   return (
-    <div>
-      <FacilityDashboardHeader title={t('aboveSiteDashboard', 'Above site facility Dashboard')} />
-    </div>
+    <Layer>
+      <FacilityDashboardHeader title={t('aboveSiteFacilityDashboard', 'Above site facility Dashboard')} />
+
+      <Button
+        className={styles.supersetBtn}
+        onClick={() => {
+          window.open(facilityDashboardAboveSiteUrl, '_blank');
+        }}>
+        {t('viewOnSuperset', 'View on Superset')}
+      </Button>
+
+      <iframe
+        className={styles.dashboard}
+        src={`${facilityDashboardAboveSiteUrl}?standalone=true`}
+        title={t('aboveSiteDashboard', 'Above Site Dashboard')}></iframe>
+    </Layer>
   );
 };
 
--- packages/esm-facility-dashboard-app/src/config-schema.ts
+++ packages/esm-facility-dashboard-app/src/config-schema.ts
@@ -1,3 +1,16 @@
-export const configSchema = {};
+import { Type } from '@openmrs/esm-framework';
 
-export interface ConfigObject {}
+export const configSchema = {
+  facilityDashboardAboveSiteUrl: {
+    _type: Type.String,
+    _description: 'Facility dashboard link for superset analytics',
+    // links for testing above site superset dashbord embeding
+    // _default: 'https://odoosuperset.kenyahmis.org/superset/dashboard/11/', // Actual facility link
+    _default: 'https://superset.datatest.ch/superset/dashboard/10/', // Public accessible test link
+    // _default: 'http://192.168.1.102:8088/superset/dashboard/1/', // Local test instance link
+  },
+};
+
+export type FacilityDashboardConfigObject = {
+  facilityDashboardAboveSiteUrl: string;
+};
