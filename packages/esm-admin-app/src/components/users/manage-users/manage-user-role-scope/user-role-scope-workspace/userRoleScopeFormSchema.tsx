dbbaa23dbff39e7171480d69e27b8c1840073fed
Ogollah <kogollah2011@gmail.com>
2025-03-07 11:36:38 +0300

    :construction: create multiple user role scopes dynamically

--- packages/esm-admin-app/src/components/users/manage-users/manage-user-role-scope/user-role-scope-workspace/userRoleScopeFormSchema.tsx
+++ packages/esm-admin-app/src/components/users/manage-users/manage-user-role-scope/user-role-scope-workspace/userRoleScopeFormSchema.tsx
@@ -5,32 +5,36 @@ const UserRoleScopeFormSchema = () => {
   const { t } = useTranslation();
 
   const userRoleScopeFormSchema = z.object({
-    userName: z.string().optional(),
-    permanent: z.boolean().optional(),
-    enabled: z.boolean().optional(),
-    stockOperation: z
-      .array(
-        z.object({
-          operationTypeName: z.string().optional(),
-          operationTypeUuid: z.string().optional(),
-        }),
-      )
-      .optional(),
-    operationLocation: z
-      .array(
-        z.object({
-          locationName: z.string().optional(),
-          locationUuid: z.string().optional(),
-        }),
-      )
-      .optional(),
-    dateRange: z
-      .object({
-        activeTo: z.date().optional(),
-        activeFrom: z.date().optional(),
-      })
-      .optional(),
-    role: z.string().optional(),
+    forms: z.array(
+      z.object({
+        userName: z.string().optional(),
+        permanent: z.boolean().optional(),
+        enabled: z.boolean().optional(),
+        operationTypes: z
+          .array(
+            z.object({
+              operationTypeName: z.string().optional(),
+              operationTypeUuid: z.string().optional(),
+            }),
+          )
+          .optional(),
+        locations: z
+          .array(
+            z.object({
+              locationName: z.string().optional(),
+              locationUuid: z.string().optional(),
+            }),
+          )
+          .optional(),
+        dateRange: z
+          .object({
+            activeTo: z.date().optional(),
+            activeFrom: z.date().optional(),
+          })
+          .optional(),
+        role: z.string().optional(),
+      }),
+    ),
   });
 
   return { userRoleScopeFormSchema };
