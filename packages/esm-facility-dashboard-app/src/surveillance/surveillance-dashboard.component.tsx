af6f4c6e03546dec51f695f4a37f15aacd3a3ab0
Laurent Ouma <lawiomosh3@gmail.com>
2025-03-14 12:59:52 +0300

    feat: Enhance surveillance dashboard with filter functionality and date range selection; update translations for new indicators

--- packages/esm-facility-dashboard-app/src/surveillance/surveillance-dashboard.component.tsx
+++ packages/esm-facility-dashboard-app/src/surveillance/surveillance-dashboard.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import FacilityDashboardHeader from '../components/header/header.component';
 import HIVPositiveNotLinkedToART from './charts/hiv-not-linked-to-art.component';
@@ -9,19 +9,21 @@ import DelayedEACCharts from './charts/delayed-eac-charts.component';
 import MissedOpportunityChart from './charts/missed-opportunity-vl-chart.component';
 import DNAPCRPendingCharts from './charts/dna-pcr-pending-chart.component';
 import HEIFinalOutcomesChart from './charts/hei-final-outcome.component';
+import { SurveillanceindicatorsFilter } from '../types';
 const SurveillancelanceDashboard = () => {
   const { t } = useTranslation();
+  const [currFilters, setCurrFilters] = useState<SurveillanceindicatorsFilter>({});
   return (
     <div>
       <FacilityDashboardHeader title={t('surveillance', 'Surveillance')} />
-      <SurveillanceFilters />
+      <SurveillanceFilters filters={currFilters} onFiltersChange={setCurrFilters} />
       <SurveillanceSummaryCards />
-      <HIVPositiveNotLinkedToART />
-      <PBFWNotInPrep />
-      <DelayedEACCharts />
-      <MissedOpportunityChart />
-      <DNAPCRPendingCharts />
-      <HEIFinalOutcomesChart />
+      {currFilters.indicator === 'getHivPositiveNotLinked' && <HIVPositiveNotLinkedToART />}
+      {currFilters.indicator === 'getPregnantPostpartumNotInPrep' && <PBFWNotInPrep />}
+      {currFilters.indicator === 'getEligibleForVlSampleNotTaken' && <DelayedEACCharts />}
+      {currFilters.indicator === 'getVirallyUnsuppressedWithoutEAC' && <MissedOpportunityChart />}
+      {currFilters.indicator === 'getHeiSixToEightWeeksWithoutPCRResults' && <DNAPCRPendingCharts />}
+      {currFilters.indicator === 'getHei24MonthsWithoutDocumentedOutcome' && <HEIFinalOutcomesChart />}
     </div>
   );
 };
