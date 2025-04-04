2245dbd845bd32351f758c894965116eedaea1bb
Ogollah <kogollah2011@gmail.com>
2025-04-04 11:59:02 +0300

    (feat) High risk pbfw charts graph (#617)
    * :construction: refactor chart layout
    
    * :construction: Add date filter implementation
    
    * Add consume pbfw values from endpoint for line graph
    
    * Add High risk PBFW Not on PrEP charts
    
    * fix chart css

--- packages/esm-facility-dashboard-app/src/surveillance/charts/hiv-not-linked-to-art.component.tsx
+++ packages/esm-facility-dashboard-app/src/surveillance/charts/hiv-not-linked-to-art.component.tsx
@@ -1,10 +1,12 @@
 import '@carbon/charts/styles.css';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import BaseIndicatorTrendChart from './base-indicator-trend-chart.component';
 import BaseArtProgressTrackingChart from './base-art-progress-tracking-chart.component';
 import useFacilityDashboardSurveillance from '../../hooks/useFacilityDashboardSurveillance';
 import { useSurveillanceData } from '../../hooks/useSurveillanceData';
+import EmptyState from '../empty-state/empty-state-log.components';
+import styles from './charts.scss';
 type HIVPositiveNotLinkedToARTProps = {
   startDate?: Date;
   endDate?: Date;
@@ -23,12 +25,24 @@ const HIVPositiveNotLinkedToART: React.FC<HIVPositiveNotLinkedToARTProps> = ({ s
 
   return (
     <>
-      <BaseIndicatorTrendChart
-        data={hivPositivePatientValue}
-        title={t('hivPositiveNotLinkedToART', 'HIV +VE Not linked to ART')}
-        yAxisTitle={t('numberTestedPositiveNotLinked', 'no tested positive not linked')}
-      />
-      <BaseArtProgressTrackingChart data={monthlyHivPositivePatientData} />
+      <div className={styles.chart}>
+        {hivPositivePatientValue.length > 0 ? (
+          <BaseIndicatorTrendChart
+            data={hivPositivePatientValue}
+            title={t('hivPositiveNotLinkedToART', 'HIV +VE Not linked to ART')}
+            yAxisTitle={t('numberTestedPositiveNotLinked', 'Number tested positive not linked')}
+          />
+        ) : (
+          <EmptyState subTitle={t('noHivPositiveNotLinked', 'No HIV +VE Not linked to ART data to display')} />
+        )}
+      </div>
+      <div className={styles.chart}>
+        {monthlyHivPositivePatientData.length > 0 ? (
+          <BaseArtProgressTrackingChart data={monthlyHivPositivePatientData} />
+        ) : (
+          <EmptyState subTitle={'No Linkage to ART data to display'} />
+        )}
+      </div>
     </>
   );
 };
