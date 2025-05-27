d0f2f58bdefc5b7c56c4247ad5d7795eae27f5b6
Donald Kibet <chelashawdonald@yahoo.com>
2025-05-27 22:20:29 +0300

    (fix) : allow non-billiable medication to be modified before dispensing

--- packages/esm-billing-app/src/billable-services/billiable-item/order-actions/components/medication-order-button.component.tsx
+++ packages/esm-billing-app/src/billable-services/billiable-item/order-actions/components/medication-order-button.component.tsx
@@ -1,17 +1,62 @@
 import React, { useCallback } from 'react';
 import { Edit } from '@carbon/react/icons';
-import { launchWorkspace } from '@openmrs/esm-framework';
+import { launchWorkspace, navigateAndLaunchWorkspace } from '@openmrs/esm-framework';
 import { BaseOrderButton } from './base-order-button.component';
 import { useMedicationOrderAction, useOrderByUuid } from '../hooks/useMedicationOrderAction';
 import { launchPrescriptionEditWorkspace } from '../hooks/useModalHandler';
 import { useTranslation } from 'react-i18next';
+import { useVisitOrOfflineVisit } from '@openmrs/esm-patient-common-lib/src';
+import { Button } from '@carbon/react';
+
 export interface MedicationOrderButtonProps {
   medicationRequestBundle?: {
     request: fhir.MedicationRequest;
+    dispenses: Array<fhir.MedicationDispense>;
   };
   actionText?: string;
   closeable?: boolean;
 }
+
+interface ModifyButtonProps {
+  currentVisit: boolean;
+  isLoading: boolean;
+  order: any;
+  patientUuid: string;
+}
+
+const ModifyButton: React.FC<ModifyButtonProps> = ({ currentVisit, isLoading, order, patientUuid }) => {
+  const { t } = useTranslation();
+
+  if (currentVisit) {
+    return (
+      <BaseOrderButton
+        size="lg"
+        kind="tertiary"
+        Icon={Edit}
+        isLoading={isLoading}
+        isDisabled={false}
+        buttonText={t('modify', 'Modify')}
+        onClick={() => launchPrescriptionEditWorkspace(order, patientUuid)}
+      />
+    );
+  }
+
+  return (
+    <Button
+      kind="danger--tertiary"
+      size="lg"
+      onClick={() =>
+        navigateAndLaunchWorkspace({
+          targetUrl: `\${openmrsSpaBase}/patient/${patientUuid}/chart`,
+          contextKey: `patient/${patientUuid}`,
+          workspaceName: 'start-visit-workspace-form',
+          additionalProps: { patientUuid },
+        })
+      }>
+      {t('activeVisitRequired', 'Start visit to modify')}
+    </Button>
+  );
+};
 
 export const MedicationOrderButton: React.FC<MedicationOrderButtonProps> = ({
   medicationRequestBundle,
@@ -30,8 +75,9 @@ export const MedicationOrderButton: React.FC<MedicationOrderButtonProps> = ({
   } = useMedicationOrderAction(medicationRequestBundle);
   const { data: order, isLoading: isOrderLoading } = useOrderByUuid(medicationRequestBundle?.request?.id);
   const isLoading = isMedicationOrderLoading && isOrderLoading;
-
+  const { currentVisit } = useVisitOrOfflineVisit(patientUuid);
   const buttonText = actionText ?? defaultButtonText;
+
   const launchModal = useCallback(() => {
     if (shouldShowBillModal) {
       launchWorkspace('create-bill-workspace', {
@@ -45,7 +91,7 @@ export const MedicationOrderButton: React.FC<MedicationOrderButtonProps> = ({
     if (dispenseFormProps) {
       launchWorkspace('dispense-workspace', dispenseFormProps);
     }
-  }, [shouldShowBillModal, medicationRequestBundle, dispenseFormProps, patientUuid, order]);
+  }, [shouldShowBillModal, medicationRequestBundle, dispenseFormProps, order]);
 
   if (!closeable) {
     return null;
@@ -54,15 +100,7 @@ export const MedicationOrderButton: React.FC<MedicationOrderButtonProps> = ({
   return (
     <div>
       {shouldAllowModify && (
-        <BaseOrderButton
-          size="lg"
-          kind="tertiary"
-          Icon={Edit}
-          isLoading={isLoading}
-          isDisabled={false}
-          buttonText={t('modify', 'Modify')}
-          onClick={() => launchPrescriptionEditWorkspace(order, patientUuid)}
-        />
+        <ModifyButton currentVisit={!!currentVisit} isLoading={isLoading} order={order} patientUuid={patientUuid} />
       )}
       <BaseOrderButton
         size="lg"
