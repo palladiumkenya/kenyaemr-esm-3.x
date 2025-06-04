f83553270acba0213f4b7b2fba5278585f3b24db
Donald Kibet <chelashawdonald@yahoo.com>
2025-06-04 22:38:16 +0300

    (feat) wrap billing metrics with `o3: View Billing Metrics` to limit who views it

--- packages/esm-billing-app/src/billing-dashboard/billing-dashboard.component.tsx
+++ packages/esm-billing-app/src/billing-dashboard/billing-dashboard.component.tsx
@@ -5,6 +5,7 @@ import BillingTabs from '../billing-tabs/billling-tabs.component';
 import MetricsCards from '../metrics-cards/metrics-cards.component';
 import styles from './billing-dashboard.scss';
 import { ClockOutStrip } from './clock-out-strip.component';
+import { UserHasAccess } from '@openmrs/esm-framework';
 
 export function BillingDashboard() {
   const { t } = useTranslation();
@@ -13,7 +14,9 @@ export function BillingDashboard() {
     <main className={styles.container}>
       <BillingHeader title={t('home', 'Home')} />
       <ClockOutStrip />
-      <MetricsCards />
+      <UserHasAccess privilege="o3: View Billing Metrics">
+        <MetricsCards />
+      </UserHasAccess>
       <BillingTabs />
     </main>
   );
