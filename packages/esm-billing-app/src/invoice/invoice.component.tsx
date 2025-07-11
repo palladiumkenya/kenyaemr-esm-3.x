bc9261f9dc4f87d5051e6746687a27343e1324d6
Donald Kibet <chelashawdonald@yahoo.com>
2025-07-11 10:56:13 +0300

    feat KHP3-8206 : enhance printing with inclusion of bill statements

--- packages/esm-billing-app/src/invoice/invoice.component.tsx
+++ packages/esm-billing-app/src/invoice/invoice.component.tsx
@@ -1,4 +1,4 @@
-import { Button, InlineLoading } from '@carbon/react';
+import { Button, InlineLoading, Popover, PopoverContent } from '@carbon/react';
 import { BaggageClaim, Close, Printer, Wallet, FolderOpen } from '@carbon/react/icons';
 import {
   defaultVisitCustomRepresentation,
@@ -30,7 +30,6 @@ import InvoiceTable from './invoice-table.component';
 import { useShaFacilityStatus } from './invoice.resource';
 import styles from './invoice.scss';
 import Payments from './payments/payments.component';
-import ReceiptPrintButton from './print-bill-receipt/receipt-print-button.component';
 import capitalize from 'lodash-es/capitalize';
 import { mutate } from 'swr';
 import startCase from 'lodash-es/startCase';
@@ -171,16 +170,6 @@ const Invoice: React.FC = () => {
       {patient && patientUuid && <ExtensionSlot name="patient-header-slot" state={{ patient, patientUuid }} />}
       <InvoiceSummary bill={bill} />
       <div className={styles.actionArea}>
-        <ReceiptPrintButton bill={bill} />
-        <Button
-          onClick={handlePrint}
-          kind="tertiary"
-          size="sm"
-          renderIcon={Printer}
-          iconDescription="Add"
-          tooltipPosition="right">
-          {t('printInvoice', 'Print invoice')}
-        </Button>
         <Button
           onClick={handleBillPayment}
           disabled={bill?.status === 'PAID'}
@@ -222,12 +211,81 @@ export function InvoiceSummary({ bill }: { readonly bill: MappedBill }) {
   };
 
   const shouldCloseBill = bill.balance === 0 && !bill.closed;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handlePrint = (documentType: string, documentTitle: string) => {
+    const dispose = showModal('print-preview-modal', {
+      onClose: () => dispose(),
+      title: documentTitle,
+      documentUrl: `/openmrs${restBaseUrl}/cashier/print?documentType=${documentType}&billId=${bill?.id}`,
+    });
+  };
 
   return (
     <>
       <div className={styles.invoiceSummary}>
         <span className={styles.invoiceSummaryTitle}>{t('invoiceSummary', 'Invoice Summary')}</span>
         <div className="invoiceSummaryActions">
+          <Popover
+            isTabTip
+            align="bottom-right"
+            onKeyDown={() => {}}
+            onRequestClose={() => setIsOpen(false)}
+            open={isOpen}>
+            <button
+              className={styles.printButton}
+              aria-expanded
+              aria-label="Settings"
+              onClick={() => setIsOpen(!isOpen)}
+              type="button">
+              <Printer />
+            </button>
+            <PopoverContent>
+              <div className={styles.popoverContent}>
+                <Button
+                  kind="ghost"
+                  size="sm"
+                  onClick={() =>
+                    handlePrint(
+                      'invoice',
+                      `${t('invoice', 'Invoice')} ${bill?.receiptNumber} - ${startCase(bill?.patientName)}`,
+                    )
+                  }
+                  renderIcon={Printer}>
+                  {t('printInvoice', 'Print Invoice')}
+                </Button>
+                {bill.balance === 0 && (
+                  <Button
+                    kind="ghost"
+                    size="sm"
+                    onClick={() => {
+                      const dispose = showModal('print-preview-modal', {
+                        onClose: () => dispose(),
+                        title: `${t('receipt', 'Receipt')} ${bill?.receiptNumber} - ${startCase(bill?.patientName)}`,
+                        documentUrl: `/openmrs${restBaseUrl}/cashier/receipt?billId=${bill.id}`,
+                      });
+                    }}
+                    renderIcon={Printer}>
+                    {t('printReceipt', 'Print Receipt')}
+                  </Button>
+                )}
+                <Button
+                  kind="ghost"
+                  size="sm"
+                  onClick={() =>
+                    handlePrint(
+                      'billstatement',
+                      `${t('billStatement', 'Bill Statement')} ${bill?.receiptNumber} - ${startCase(
+                        bill?.patientName,
+                      )}`,
+                    )
+                  }
+                  renderIcon={Printer}>
+                  {t('printBillStatement', 'Print Bill Statement')}
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
           {shouldCloseBill && (
             <UserHasAccess privilege="Close Cashier Bills">
               <Button
