2e7e92cbc5ac8f650073dcf792fd6f729dc1c264
Donald Kibet <chelashawdonald@yahoo.com>
2025-08-11 08:51:40 +0300

    feat : Add ability to configure which links are shown and point to 2.x reports (#733)
    Co-authored-by: Fredrick Kioko <67967749+its-kios09@users.noreply.github.com>

--- packages/esm-patient-flags-app/src/config-schema.ts
+++ packages/esm-patient-flags-app/src/config-schema.ts
@@ -1,5 +1,17 @@
-import { Type } from '@openmrs/esm-framework';
+import { ConfigSchema, Type } from '@openmrs/esm-framework';
 
-export const configSchema = {};
+export const configSchema: ConfigSchema = {
+  excludeLinks: {
+    _type: Type.Array,
+    _elements: {
+      _type: Type.String,
+      _description: 'Label for the link to hide',
+    },
+    _default: [],
+    _description: 'Array of links to hide in the patient flags app',
+  },
+};
 
-export type ConfigObject = {};
+export type ConfigObject = {
+  excludeLinks: Array<string>;
+};
