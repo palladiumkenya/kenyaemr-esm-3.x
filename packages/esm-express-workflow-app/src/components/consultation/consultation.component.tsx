426ada503dded262b363fe6e971604e7f1caad8f
Laurent Ouma <lawiomosh3@gmail.com>
2025-08-27 22:47:12 +0300

    fix conultation spelling

---
+++ packages/esm-express-workflow-app/src/components/consultation/consultation.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ConsultationProps = {
+  dashboardTitle: string;
+};
+
+const Consultation: React.FC<ConsultationProps> = () => {
+  return <div>Consulation</div>;
+};
+
+export default Consultation;
---
+++ packages/esm-express-workflow-app/src/components/consultation/dashboard.component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { spaBasePath } from '../../constants';
+import PatientChart from '../../shared/patient-chart/patient-chart.component';
+import Consultation from './consultation.component';
+
+type ConsultationDashboardProps = {
+  dashboardTitle: string;
+};
+
+const ConsultationDashboard: React.FC<ConsultationDashboardProps> = ({ dashboardTitle }) => {
+  return (
+    <BrowserRouter basename={`${spaBasePath}/consultation`}>
+      <Routes>
+        <Route path="/" element={<Consultation dashboardTitle={dashboardTitle} />} />
+        <Route path="/:patientUuid" element={<PatientChart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default ConsultationDashboard;
