b031b2a38af8700656c5c324890526b0fd35f099
Felix Kiprotich <fkiprotich845@gmail.com>
2025-08-28 09:39:09 +0300

    added procedure date filter context

--- packages/esm-express-workflow-app/src/procedures/procedures.component.tsx
+++ packages/esm-express-workflow-app/src/procedures/procedures.component.tsx
@@ -1,7 +1,15 @@
-import { ExtensionSlot } from '@openmrs/esm-framework';
-import React from 'react';
+import { ExtensionSlot, useDefineAppContext } from '@openmrs/esm-framework';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
 
+export type DateFilterContext = {
+  dateRange: Array<Date>;
+  setDateRange: React.Dispatch<React.SetStateAction<Array<Date>>>;
+};
 const Procedures: React.FC = () => {
+  const [dateRange, setDateRange] = useState<Date[]>([dayjs().startOf('day').toDate(), new Date()]);
+  useDefineAppContext<DateFilterContext>('procedures-date-filter', { dateRange, setDateRange });
+
   return (
     <div>
       <ExtensionSlot name="procedures-dashboard-slot" />
