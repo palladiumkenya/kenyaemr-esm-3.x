f1a693956f97029f9a4d6ab34453aa541af735c4
Laurent Ouma <lawiomosh3@gmail.com>
2025-08-28 08:26:20 +0300

    Implement consultation comonent

--- packages/esm-express-workflow-app/src/components/consultation/consultation.component.tsx
+++ packages/esm-express-workflow-app/src/components/consultation/consultation.component.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-
+import { useTranslation } from 'react-i18next';
+import { useQueues } from '../../hooks/useServiceQueues';
+import { InlineLoading } from '@carbon/react';
+import { HomePictogram, PageHeader } from '@openmrs/esm-framework';
+import styles from './consultation.scss';
+import Card from '../../shared/cards/card.component';
+import capitalize from 'lodash-es/capitalize';
+import QueueTab from '../../shared/queue/queue-tab.component';
 type ConsultationProps = {
   dashboardTitle: string;
 };
 
-const Consultation: React.FC<ConsultationProps> = () => {
-  return <div>Consulation</div>;
+const Consultation: React.FC<ConsultationProps> = ({ dashboardTitle }) => {
+  const { t } = useTranslation();
+  const { queues, isLoading, error } = useQueues();
+  const consultationQueues = queues.filter((queue) => queue.name.toLowerCase().includes('consultation'));
+
+  if (isLoading) {
+    return <InlineLoading description={t('loadingQueues', 'Loading queues...')} />;
+  }
+  return (
+    <div>
+      <PageHeader className={styles.pageHeader} title={capitalize(dashboardTitle)} illustration={<HomePictogram />} />
+      <div>
+        <div className={styles.cards}>
+          <Card title={t('awaitingConsultation', 'Awaiting consultation')} value={queues.length.toString()} />
+          <Card title={t('awaitingInvestigation', 'Awaiting Investigation')} value={queues.length.toString()} />
+          <Card title={t('investigationComplete', 'Investigation complete')} value={queues.length.toString()} />
+          <Card title={t('visitComplete', 'Visit complete')} value={queues.length.toString()} />
+        </div>
+        <QueueTab queues={consultationQueues} />
+      </div>
+    </div>
+  );
 };
 
 export default Consultation;
