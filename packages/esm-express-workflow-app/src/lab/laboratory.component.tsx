95cb2fd730fcf5e7b868ced010a84b9345b93e99
Felix Kiprotich <fkiprotich845@gmail.com>
2025-08-28 09:25:11 +0300

    injected radiology,lab and procedures

--- packages/esm-express-workflow-app/src/lab/laboratory.component.tsx
+++ packages/esm-express-workflow-app/src/lab/laboratory.component.tsx
@@ -1,7 +1,13 @@
+import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
 
 const Laboratory: React.FC = () => {
-  return <div>Laboratory</div>;
+  return (
+    <div>
+      {/* This slot will host the laboratory app */}
+      <ExtensionSlot name="laboratory-dashboard-slot" />
+    </div>
+  );
 };
 
 export default Laboratory;
