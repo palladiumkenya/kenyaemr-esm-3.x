6857179cea0a3fe6ab78bbffd3e6a5c697eb88e1
makombe <kennedymakombe@gmail.com>
2025-08-28 12:08:54 +0300

    Wire Accounting dashboard

--- packages/esm-express-workflow-app/src/components/accounting/accounting.component.tsx
+++ packages/esm-express-workflow-app/src/components/accounting/accounting.component.tsx
@@ -1,7 +1,8 @@
+import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
 
 const Accounting: React.FC = () => {
-  return <div>Accounting</div>;
+  return <ExtensionSlot name="accounting-dashboard-slot" />;
 };
 
 export default Accounting;
