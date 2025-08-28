b30222b19aa67217daa5fd0d48ed32e13a3e6b05
stephen.chege <njorocs@gmail.com>
2025-08-28 14:41:09 +0300

    Mch UI revision (#766)
    * Added UI for MCH triage and consultation
    
    * Fixed lodash import from wild import to specific lib
    
    ---------
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>

--- packages/esm-express-workflow-app/src/components/mch/index.tsx
+++ packages/esm-express-workflow-app/src/components/mch/index.tsx
@@ -1,15 +1,15 @@
 import { getSyncLifecycle } from '@openmrs/esm-framework';
 
 import { moduleName } from '../../constants';
-import MCH from './mch.component';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
+import MCHDashboard from './dashboard.component';
 
 const options = {
   featureName: 'express-workflow',
   moduleName,
 };
 
-export const mchDashboard = getSyncLifecycle(MCH, options);
+export const mchDashboard = getSyncLifecycle(MCHDashboard, options);
 export const mchLeftPanelLink = getSyncLifecycle(
   createDashboardLink({
     title: 'MCH',
---
+++ packages/esm-express-workflow-app/src/components/mch/mch.consultation.component.tsx
@@ -0,0 +1,33 @@
+import { InlineLoading } from '@carbon/react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useQueues } from '../../hooks/useServiceQueues';
+import Card from '../../shared/cards/card.component';
+import QueueTab from '../../shared/queue/queue-tab.component';
+import styles from './mch.scss';
+type ConsultationProps = {
+  dashboardTitle: string;
+};
+
+const MCHConsultation: React.FC = () => {
+  const { t } = useTranslation();
+  const { queues, isLoading, error } = useQueues();
+  const consultationQueues = queues.filter((queue) => queue.name.toLowerCase().includes('consultation'));
+
+  if (isLoading) {
+    return <InlineLoading description={t('loadingQueues', 'Loading queues...')} />;
+  }
+  return (
+    <div>
+      <div className={styles.cards}>
+        <Card title={t('awaitingConsultation', 'Awaiting consultation')} value={'20'} />
+        <Card title={t('awaitingInvestigation', 'Awaiting Investigation')} value={'5'} />
+        <Card title={t('investigationComplete', 'Investigation complete')} value={'7'} />
+        <Card title={t('visitComplete', 'Visit complete')} value={'4'} />
+      </div>
+      <QueueTab queues={consultationQueues} />
+    </div>
+  );
+};
+
+export default MCHConsultation;
