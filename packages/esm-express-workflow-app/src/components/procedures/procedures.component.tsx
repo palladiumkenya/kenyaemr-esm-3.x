14faceff1674a2f62e4c911131d933330e65781b
Felix Kiprotich <72152368+FelixKiprotich350@users.noreply.github.com>
2025-08-28 14:34:00 +0300

    Injected lab and radioology apps (#763)
    * inject 3 apps
    
    * reinstalled yarn
    
    ---------
    
    Co-authored-by: Makombe Kennedy <kennedymakombe@gmail.com>

--- packages/esm-express-workflow-app/src/components/procedures/procedures.component.tsx
+++ packages/esm-express-workflow-app/src/components/procedures/procedures.component.tsx
@@ -1,7 +1,12 @@
+import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
 
 const Procedures: React.FC = () => {
-  return <div>Procedures</div>;
+  return (
+    <div>
+      <ExtensionSlot name="procedures-dashboard-slot" />
+    </div>
+  );
 };
 
 export default Procedures;
