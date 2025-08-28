0f5ffd6afd9accd46276799d87a2eb1e5d0e736c
Donald Kibet <chelashawdonald@yahoo.com>
2025-08-28 11:47:55 +0300

    fix : update yarn lock to fix ci fail build (#759)
    * fix : update yarn lock to fix ci fail build
    
    * fix : correct types to fix ci

--- packages/esm-patient-clinical-view-app/src/specialized-clinics/hiv-care-and-treatment-services/hiv-testing-services/views/hiv-testing/hiv-testing-services.component.tsx
+++ packages/esm-patient-clinical-view-app/src/specialized-clinics/hiv-care-and-treatment-services/hiv-testing-services/views/hiv-testing/hiv-testing-services.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ContentSwitcher, Switch, SwitchEventHandlersParams } from '@carbon/react';
+import { ContentSwitcher, Switch } from '@carbon/react';
 import styles from '../../hiv-testing-component.scss';
 import { useTranslation } from 'react-i18next';
 import HivScreeningEncounters from './tabs/hiv-screening.component';
@@ -12,20 +12,23 @@ interface OverviewListProps {
 
 const HivTestingEncountersList: React.FC<OverviewListProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
-  const [switchItem, setSwitcherItem] = useState<SwitchEventHandlersParams>({ index: 0 });
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <div className={styles.widgetCard}>
       <CardHeader title={t('htsClinicalView', 'HTS Clinical View')}>
         <div className={styles.contextSwitcherContainer}>
-          <ContentSwitcher size="md" selectedIndex={switchItem?.index} onChange={setSwitcherItem}>
+          <ContentSwitcher
+            size="md"
+            selectedIndex={selectedIndex}
+            onChange={({ index }) => setSelectedIndex(index ?? 0)}>
             <Switch name={'screening'} text="Screening" />
             <Switch name={'hivTesting'} text="Testing" />
           </ContentSwitcher>
         </div>
       </CardHeader>
-      {<>{switchItem.index == 0 && <HivScreeningEncounters patientUuid={patientUuid} />}</>}
-      {<>{switchItem.index == 1 && <HivTestingEncounters patientUuid={patientUuid} />}</>}
+      {<>{selectedIndex == 0 && <HivScreeningEncounters patientUuid={patientUuid} />}</>}
+      {<>{selectedIndex == 1 && <HivTestingEncounters patientUuid={patientUuid} />}</>}
     </div>
   );
 };
