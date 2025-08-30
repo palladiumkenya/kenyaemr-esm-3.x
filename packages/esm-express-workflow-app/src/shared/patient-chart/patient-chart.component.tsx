a4fccee3626bafd2780acf585a0067b6f42edf3c
Donald Kibet <chelashawdonald@yahoo.com>
2025-08-30 15:11:09 +0300

    feat : Add ability to launch forms from express-workflows

--- packages/esm-express-workflow-app/src/shared/patient-chart/patient-chart.component.tsx
+++ packages/esm-express-workflow-app/src/shared/patient-chart/patient-chart.component.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import { ErrorState, ExtensionSlot, fetchCurrentPatient } from '@openmrs/esm-framework';
+import React, { useEffect, useMemo } from 'react';
+import {
+  ErrorState,
+  ExtensionSlot,
+  fetchCurrentPatient,
+  setCurrentVisit,
+  WorkspaceContainer,
+} from '@openmrs/esm-framework';
 import { useParams } from 'react-router-dom';
 import styles from './patient-chart.scss';
 import { InlineLoading, Tab, TabList, TabPanel, TabPanels, Tabs } from '@carbon/react';
@@ -16,6 +22,17 @@ const PatientChart: React.FC = () => {
     error,
   } = useSWR(patientUuid ? ['patient', patientUuid] : null, () => fetchCurrentPatient(patientUuid!, {}));
 
+  const state = useMemo(() => ({ patient, patientUuid }), [patient, patientUuid]);
+
+  useEffect(() => {
+    if (patientUuid) {
+      setCurrentVisit(patientUuid, null);
+    }
+    return () => {
+      setCurrentVisit(null, null);
+    };
+  }, [patientUuid]);
+
   if (isLoading) {
     return <InlineLoading description={t('loadingPatient', 'Loading patient...')} />;
   }
@@ -30,18 +47,24 @@ const PatientChart: React.FC = () => {
       <div className={styles.tabsContainer}>
         <Tabs>
           <TabList contained>
-            <Tab>{t('vitalsAndAnthropometrics', 'Vitals and Anthropometrics')}</Tab>
+            <Tab>{t('vitalsAndAnthropometric', 'Vitals and Anthropometric')}</Tab>
             <Tab>{t('programManagement', 'Program Management')}</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
-              <ExtensionSlot name="vitals-and-anthropometrics-slot" state={{ patient, patientUuid }} />
+              <ExtensionSlot name="vitals-and-anthropometric-slot" state={state} />
             </TabPanel>
             <TabPanel>
-              <ExtensionSlot name="program-management-slot" state={{ patient, patientUuid }} />
+              <ExtensionSlot name="program-management-slot" state={state} />
             </TabPanel>
           </TabPanels>
         </Tabs>
+        <WorkspaceContainer
+          showSiderailAndBottomNav
+          key="express-workflow"
+          contextKey={`express-workflow/triage/${patientUuid}`}
+          additionalWorkspaceProps={state}
+        />
       </div>
     </div>
   );
