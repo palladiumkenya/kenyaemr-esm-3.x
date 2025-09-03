cec88d6c37870ec08c2537c41fb98aed893551e2
Donald Kibet <chelashawdonald@yahoo.com>
2025-09-03 10:17:28 +0300

    Revert "(feat) : Add ability to launch forms from ADR (#777)" (#778)
    This reverts commit 7af7d7fa88f78be529e499417318d772d8b64cc8.

--- packages/esm-adr-app/src/components/dashboard/dashboard-view.component.tsx
+++ packages/esm-adr-app/src/components/dashboard/dashboard-view.component.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  ExtensionSlot,
-  PageHeader,
-  AppointmentsPictogram,
-  PageHeaderContent,
-  PharmacyPictogram,
-} from '@openmrs/esm-framework';
+import { ExtensionSlot, PageHeader, AppointmentsPictogram, PageHeaderContent } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
 import styles from './dashboard-view.scss';
 const DashboardView: React.FC<{ dashboardSlot: string; title: string }> = ({ dashboardSlot, title }) => {
@@ -13,7 +7,7 @@ const DashboardView: React.FC<{ dashboardSlot: string; title: string }> = ({ das
   return (
     <>
       <PageHeader className={styles.pageHeader}>
-        <PageHeaderContent title={t('adrAssessment', 'ADR Assessment')} illustration={<PharmacyPictogram />} />
+        <PageHeaderContent title={t('adrAssessment', 'ADR Assessment')} illustration={<AppointmentsPictogram />} />
       </PageHeader>
       <ExtensionSlot name={dashboardSlot} state={{ dashboardTitle: title }} />
     </>
