e04fd1141d8adfb90e9821ea8b8d33a189455bb6
Donald Kibet <chelashawdonald@yahoo.com>
2025-09-16 11:19:02 +0300

    feat : implement `clinical-encounter-view` on patient-chart

--- packages/esm-express-workflow-app/src/components/consultation/index.ts
+++ packages/esm-express-workflow-app/src/components/consultation/index.ts
@@ -1,8 +1,10 @@
 import { getSyncLifecycle } from '@openmrs/esm-framework';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
 import { moduleName } from '../../constants';
 import ConsultationDashboard from './dashboard.component';
 import { createLeftPanelLink } from '../../shared/dashboard-link/dashboard-link.component';
+import ClinicalEncounter from './clinical-encounter/clinical-encounter.component';
 
 const options = {
   featureName: 'express-workflow',
@@ -14,6 +16,17 @@ export const consultationLeftPanelLink = getSyncLifecycle(
   createLeftPanelLink({
     title: 'Consultation',
     name: 'consultation',
+  }),
+  options,
+);
+
+export const clinicalEncounter = getSyncLifecycle(ClinicalEncounter, options);
+// TODO: register Stethoscope icon in the icon registry
+export const clinicalEncounterLink = getSyncLifecycle(
+  createDashboardLink({
+    title: 'Clinical Encounter',
+    path: 'clinical-encounter',
+    icon: 'omrs-icon-syringe',
   }),
   options,
 );
