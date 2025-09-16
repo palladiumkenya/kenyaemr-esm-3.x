bcf147da76052db1dd9fc4d6ea5bff5e216d473e
Donald Kibet <chelashawdonald@yahoo.com>
2025-09-16 11:42:04 +0300

    feat : implement `clinical-encounter-view` on patient-chart (#803)

--- packages/esm-express-workflow-app/src/shared/patient-chart/patient-summary-dashboard/patient-summary-dashboard.component.tsx
+++ packages/esm-express-workflow-app/src/shared/patient-chart/patient-summary-dashboard/patient-summary-dashboard.component.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Tabs, TabList, Tab, TabPanels, TabPanel, Layer, Grid, Column } from '@carbon/react';
+import { Layer, Grid, Column } from '@carbon/react';
 import {
   CloudMonitoring,
   Activity,
@@ -10,8 +10,8 @@ import {
   Attachment,
 } from '@carbon/react/icons';
 import { useTranslation } from 'react-i18next';
-import { ExtensionSlot } from '@openmrs/esm-framework';
 import styles from './patient-summary-dashboard.scss';
+import ExtensionTabs, { ExtensionTabItem } from '../../tabs/extension-tabs.component';
 
 type PatientSummaryDashboardProps = {
   patientUuid: string;
@@ -22,44 +22,56 @@ const PatientSummaryDashboard: React.FC<PatientSummaryDashboardProps> = ({ patie
   const { t } = useTranslation();
 
   const state = useMemo(() => ({ patientUuid, patient }), [patientUuid, patient]);
+  const items: Array<ExtensionTabItem> = [
+    {
+      label: t('patientSummary', 'Patient Summary'),
+      icon: Dashboard,
+      slotName: 'ewf-patient-summary-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('vitalsAndAnthropometric', 'Vitals & Anthropometric'),
+      icon: Activity,
+      slotName: 'ewf-vitals-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('carePanel', 'Care Panel'),
+      icon: CloudMonitoring,
+      slotName: 'ewf-care-panel-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('immunizations', 'Immunizations'),
+      icon: IbmWatsonDiscovery,
+      slotName: 'ewf-immunizations-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('relationships', 'Relationships'),
+      icon: Settings,
+      slotName: 'ewf-relationships-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('appointments', 'Appointments'),
+      icon: Calendar,
+      slotName: 'ewf-appointments-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+    {
+      label: t('attachments', 'Attachments'),
+      icon: Attachment,
+      slotName: 'ewf-attachments-slot',
+      slotClassName: styles.ewfExtensionSlot,
+    },
+  ];
+
   return (
     <Layer>
       <Grid condensed>
         <Column lg={16} md={8} sm={4}>
-          <Tabs>
-            <TabList contained>
-              <Tab renderIcon={Dashboard}>{t('patientSummary', 'Patient Summary')}</Tab>
-              <Tab renderIcon={Activity}>{t('vitalsAndAnthropometric', 'Vitals & Anthropometric')}</Tab>
-              <Tab renderIcon={CloudMonitoring}>{t('carePanel', 'Care Panel')}</Tab>
-              <Tab renderIcon={IbmWatsonDiscovery}>{t('immunizations', 'Immunizations')}</Tab>
-              <Tab renderIcon={Settings}>{t('relationships', 'Relationships')}</Tab>
-              <Tab renderIcon={Calendar}>{t('appointments', 'Appointments')}</Tab>
-              <Tab renderIcon={Attachment}>{t('attachments', 'Attachments')}</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-patient-summary-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-vitals-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-care-panel-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-immunizations-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-relationships-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-appointments-slot" state={state} />
-              </TabPanel>
-              <TabPanel>
-                <ExtensionSlot className={styles.ewfExtensionSlot} name="ewf-attachments-slot" state={state} />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          <ExtensionTabs items={items} state={state} />
         </Column>
       </Grid>
     </Layer>
