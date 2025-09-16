7f7d899441bc252b4d7de638adb6a3109a8b1b22
Donald Kibet <chelashawdonald@yahoo.com>
2025-09-16 20:16:50 +0300

    feat : implement pharmacy, hiv-care-and-treatment patient chart for DHA

--- packages/esm-express-workflow-app/src/components/pharmacy/index.ts
+++ packages/esm-express-workflow-app/src/components/pharmacy/index.ts
@@ -3,6 +3,8 @@ import { getSyncLifecycle } from '@openmrs/esm-framework';
 import Pharmacy from './pharmacy.component';
 import { moduleName } from '../../constants';
 import { createLeftPanelLink } from '../../shared/dashboard-link/dashboard-link.component';
+import PharmacyTabs from './pharmacy-tabs.component';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib/src';
 
 const options = {
   featureName: 'express-workflow',
@@ -17,3 +19,12 @@ export const pharmacyLink = getSyncLifecycle(
   options,
 );
 export const pharmacy = getSyncLifecycle(Pharmacy, options);
+export const pharmacyTabs = getSyncLifecycle(PharmacyTabs, options);
+export const pharmacyPatientChartDashboardLink = getSyncLifecycle(
+  createDashboardLink({
+    path: 'pharmacy',
+    title: 'Pharmacy',
+    icon: 'omrs-icon-medication',
+  }),
+  options,
+);
