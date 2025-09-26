856d85fc2fa0c274fa4218b341fc279995d5205e
Fredrick Kioko <67967749+its-kios09@users.noreply.github.com>
2025-09-26 09:57:53 +0300

    (feat) Migrate lab manifest to the Applink and fix navigation issues (#814)
    * (feat) Migrate lab manifest to the Applink and fix navigation issues
    
    * (chore) Removed the test component

--- packages/esm-lab-manifest-app/src/root.component.tsx
+++ packages/esm-lab-manifest-app/src/root.component.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LabManifestComponent from './component/lab-manifest.component';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import LabManifestDetail from './component/lab-manifest-detail.component';
-import LabManifestOverview from './component/lab-manifest-overview.component';
+import { useConfig, useLeftNav } from '@openmrs/esm-framework';
+import { LabManifestConfig } from './config-schema';
+import Dashboard from './dashboard/home-dashboard.component';
+
+const LeftNavController: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+  const spaBasePath = `${window.spaBase}/lab-manifest`;
+  const { leftNavMode: configLeftNavMode } = useConfig<LabManifestConfig>();
+  const [currentLeftNavMode, setCurrentLeftNavMode] = useState<'normal' | 'collapsed' | 'hidden'>(configLeftNavMode);
+
+  useEffect(() => {
+    const isDetailPage = location.pathname.includes('/lab-manifest/detail/');
+
+    if (isDetailPage) {
+      setCurrentLeftNavMode('hidden');
+    } else {
+      setCurrentLeftNavMode(configLeftNavMode);
+    }
+  }, [location.pathname, configLeftNavMode]);
+
+  useLeftNav({
+    name: 'lab-manifest-page-dashboard-slot',
+    basePath: spaBasePath,
+    mode: currentLeftNavMode,
+  });
+
+  return <>{children}</>;
+};
 
 const Root: React.FC = () => {
-  const baseName = window.getOpenmrsSpaBase() + 'home/lab-manifest';
-
   return (
-    <BrowserRouter basename={baseName}>
-      <Routes>
-        <Route path="/" element={<LabManifestComponent />} />
-        <Route path="/overview" element={<LabManifestOverview />} />
-        <Route path="/:manifestUuid" element={<LabManifestDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <main>
+      <BrowserRouter basename={window.spaBase}>
+        <LeftNavController>
+          <Routes>
+            <Route path="/lab-manifest/detail/:manifestUuid" element={<LabManifestDetail />} />
+            <Route path="/lab-manifest/:dashboard" element={<Dashboard />} />
+            <Route path="/lab-manifest" element={<Dashboard />} />
+          </Routes>
+        </LeftNavController>
+      </BrowserRouter>
+    </main>
   );
 };
 
