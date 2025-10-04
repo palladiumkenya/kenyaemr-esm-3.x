ae9dc225354b8b4d8bfe257a82462923d52eb1c5
Laurent Ouma <lawiomosh3@gmail.com>
2025-10-04 15:39:46 +0300

    feat: Add new program forms for TB, Violence screening, TPT, PrEP, NCD, KVP, and HIV Program

--- packages/esm-care-panel-app/src/care-programs/care-program.resource.ts
+++ packages/esm-care-panel-app/src/care-programs/care-program.resource.ts
@@ -58,15 +58,17 @@ const programForms = [
     programUuid: '72635673-0613-4259-916e-e0d5d5ef8f66',
     forms: [
       { formName: 'ANC Follow Up form', formUuId: '6fb1a39b-0a57-4239-afd7-a5490d281cb9' },
+      { formName: 'MCH Antenatal Visit', formUuId: 'e8f98494-af35-4bb8-9fc7-c409c8fed843' },
       { formName: 'Antenatal Care (ANC) Discontinuation', formUuId: '38885518-c71a-4661-8edf-3db67707e1d1' },
     ],
   },
   {
-    programName: 'MCH - Child Services',
+    programName: 'MCH - Child Services', // cwc
     programUuid: 'c2ecdf11-97cd-432a-a971-cfd9bd296b83',
     forms: [
-      { formName: 'Child welafare clinic form', formUuId: '755b59e6-acbb-4853-abaf-be302039f902' },
+      { formName: 'Child welfare clinic form', formUuId: '755b59e6-acbb-4853-abaf-be302039f902' }, // CWC followup
       { formName: 'MCH Postnatal Visit Form', formUuId: '72aa78e0-ee4b-47c3-9073-26f3b9ecc4a7' },
+      { formName: 'Child Welfare Services Discontinuation', formUuId: '1dd02c43-904b-4206-8378-7b1a8414c326' },
     ],
   },
   {
@@ -85,12 +87,109 @@ const programForms = [
       { formName: 'Family Planning Discontinuation', formUuId: 'efc782ea-9a16-4791-824a-18be7417eda4' },
     ],
   },
+  {
+    programName: 'TB',
+    programUuid: '9f144a34-3a4a-44a9-8486-6b7af6cc64f6',
+    forms: [
+      { formName: 'TB Enrollment', formUuId: '89994550-9939-40f3-afa6-173bce445c79' },
+      { formName: 'TB FollowUp', formUuId: '2daabb77-7ad6-4952-864b-8d23e109c69d' },
+      { formName: 'TB Discontinuation', formUuId: '4b296dd0-f6be-4007-9eb8-d0fd4e94fb3a' },
+    ],
+  },
+  {
+    programName: 'Violence screening',
+    programUuid: 'e41c3d74-37c7-4001-9f19-ef9e35224b70',
+    forms: [
+      { formName: 'Violence enrollment', formUuId: '9ba1d4aa-57d7-48f9-a635-a23508e8136c' },
+      { formName: 'Violence Screening', formUuId: '03767614-1384-4ce3-aea9-27e2f4e67d01' },
+      { formName: 'Violence Reporting Form', formUuId: '10cd2ca0-8d25-4876-b97c-b568a912957e' },
+      { formName: 'Violence Consent Form', formUuId: 'd720a8b3-52cc-41e2-9a75-3fd0d67744e5' },
+      // { formName: 'Sexual Violence post rape care 363A', formUuId: '' }, // TODO: not found
+      // { formName: 'Sexual Violence PRC Psychological Assessment 363B', formUuId: '' }, // TODO: not found
+      { formName: 'Physical and Emotional Violence Form', formUuId: 'a0943862-f0fe-483d-9f11-44f62abae063' },
+      { formName: 'Violence Trauma Counselling', formUuId: 'e983d758-5adf-4917-8172-0f4be4d8116a' },
+      { formName: 'Violence Community Linkage Form', formUuId: 'f760e38c-3d2f-4a5d-aa3d-e9682576efa8' },
+      { formName: 'Violence Legal Form', formUuId: 'd0c36426-4503-4236-ab5d-39bff77f2b50' },
+      { formName: 'Violence Perpetrator Details', formUuId: 'f37d7e0e-95e8-430d-96a3-8e22664f74d6' },
+      { formName: 'PEP FOLLOWUP Form', formUuId: '155ccbe2-a33f-4a58-8ce6-57a7372071ee' },
+      { formName: 'Violence Discontinuation Form', formUuId: '8fed3d06-f8a1-4cb8-b853-cd93394bab79' },
+    ],
+  },
+  {
+    programName: 'TPT',
+    programUuid: '335517a1-04bc-438b-9843-1ba49fb7fcd9',
+    forms: [
+      { formName: 'TPT Initiation', formUuId: '61ea2a72-b0f9-47cf-ae86-443f88656acc' },
+      { formName: 'TPT FollowUp', formUuId: '9d0e4be8-ab72-4394-8df7-b509b9d45179' },
+      { formName: 'TPT Outcome/Discontinuation', formUuId: '5bdd3b65-8b7b-46a0-9f7b-dfe764143848' },
+    ],
+  },
+  {
+    programName: 'PrEP',
+    programUuid: '214cad1c-bb62-4d8e-b927-810a046daf62',
+    forms: [
+      { formName: 'PrEP Initial Form', formUuId: '1bfb09fc-56d7-4108-bd59-b2765fd312b8' },
+      { formName: 'PrEP INITIATION', formUuId: 'd5ca78be-654e-4d23-836e-a934739be555' },
+      { formName: 'PrEP Follow Up', formUuId: 'ee3e2017-52c0-4a54-99ab-ebb542fb8984' },
+      { formName: 'PrEP Monthly Refill Form', formUuId: '291c03c8-a216-11e9-a2a3-2a2ae2dbcce4' },
+    ],
+  },
+  {
+    programName: 'NCD',
+    programUuid: 'ffee43c4-9ccd-4e55-8a70-93194e7fafc6',
+    forms: [
+      { formName: 'NCD Initial Form', formUuId: 'c4994dd7-f2b6-4c28-bdc7-8b1d9d2a6a97' },
+      { formName: 'NCD Follow Up', formUuId: '3e1057da-f130-44d9-b2bb-53e039b953c6' },
+      // { formName: 'NCD Discontinuation', formUuId: '' }, // TODO Form not found
+    ],
+  },
+  {
+    // TODO link to peer educator
+    // TODO Update peer calendar
+    programName: 'KVP',
+    programUuid: '7447305a-18a7-11e9-ab14-d663bd873d93',
+    forms: [
+      { formName: 'KVP Contact Form', formUuId: '185dec84-df6f-4fc7-a370-15aa8be531ec' },
+      { formName: 'KVP Clinical Enrollment', formUuId: 'c7f47cea-207b-11e9-ab14-d663bd873d93' },
+      { formName: 'KVP Clinical Encounter form', formUuId: '92e041ac-9686-11e9-bc42-526af7764f64' },
+      { formName: 'KVP Client Discontinuation', formUuId: '1f76643e-2495-11e9-ab14-d663bd873d93' },
+    ],
+  },
+  {
+    // TODO, lINK TO CASE MANAGENER
+    programName: 'HIV Program',
+    programUuid: 'dfdc6d40-2f2f-463d-ba90-cc97350441a8',
+    forms: [
+      { formName: 'HIV Enrollment', formUuId: 'e4b506c1-7379-42b6-a374-284469cba8da' },
+      { formName: 'ART Readyness', formUuId: '782a4263-3ac9-4ce8-b316-534571233f12' }, // USING ART prepairation form (closest to readiness)
+      { formName: 'HIV Green Card', formUuId: '22c68f86-bbf0-49ba-b2d1-23fa7ccf0259' },
+      { formName: 'HIV Discontinuation', formUuId: 'e3237ede-fa70-451f-9e6c-0908bc39f8b9' },
+    ],
+  },
   // { // TODO Uncomment and add conrect uuids after program is added by the BA
   //   programName: 'Pre-Conception care program',
   //   programUuid: '191269d2-9973-4958-9936-f687ed771050',
   //   forms: [
   //     { formName: 'Family Planning Form', formUuId: 'a52c57d4-110f-4879-82ae-907b0d90add6' },
   //     { formName: 'Family Planning Discontinuation', formUuId: 'efc782ea-9a16-4791-824a-18be7417eda4' },
+  //   ],
+  // },
+  // {
+  //   // TODO Uncomment and add conrect uuids after program is added by the BA
+  //   programName: 'Medical Assited therapy',
+  //   programUuid: '191269d2-9973-4958-9936-f687ed771050',
+  //   forms: [
+  //     {
+  //       formName: 'MAT Enrollment Form',
+  //       formUuId: '',
+  //     }, // TODO:Form not found
+  //     { formName: 'MAT Initial Registration Form', formUuId: '9a9cadd7-fba1-4a24-94aa-43edfbecf8d9' },
+  //     { formName: 'MAT Clinical Encounter Form', formUuId: '5ed937a0-0933-41c3-b638-63d8a4779845' },
+  //     { formName: 'MAT Psycho-social Intake & Follow-up Form', formUuId: 'cfd2109b-63b3-43de-8bb3-682e80c5a965' },
+  //     { formName: 'MAT Psychiatric Intake and Follow up Form', formUuId: 'fdea46a1-9423-4ef9-b780-93b32b48a528' },
+  //     { formName: 'MAT Transit/Referral Form', formUuId: 'b9495048-eceb-4dd2-bfba-330dc4900ee9' },
+  //     { formName: 'MAT Cessation Form', formUuId: 'fa58cbc1-91c8-4920-813b-fde7fd69533b' },
+  //     { formName: 'MAT Discontinuation Form', formUuId: '38d6e116-b96c-4916-a821-b4dc83e2041d' },
   //   ],
   // },
 ];
