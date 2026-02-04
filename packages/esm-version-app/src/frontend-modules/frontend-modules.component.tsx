e4598a97a0b9b05444eb3a5de45b0258cd75bc25
Donald Kibet <chelashawdonald@yahoo.com>
2026-02-04 10:19:59 +0300

    (enhc) : Make system page configurable

--- packages/esm-version-app/src/frontend-modules/frontend-modules.component.tsx
+++ packages/esm-version-app/src/frontend-modules/frontend-modules.component.tsx
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { DataTable, Table, TableHead, TableRow, TableHeader, TableBody, TableCell } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
+
 import { useFrontendModules } from '../hooks/useFrontendModules';
-import {
-  StructuredListWrapper,
-  StructuredListHead,
-  StructuredListRow,
-  StructuredListCell,
-  StructuredListBody,
-} from '@carbon/react';
 
 const FrontendModule: React.FC = () => {
+  const { t } = useTranslation();
   const installedModules = useFrontendModules();
+
+  const headers = [
+    { key: 'name', header: t('moduleName', 'Module name') },
+    { key: 'version', header: t('version', 'Version') },
+  ];
+  const rows = useMemo(
+    () =>
+      installedModules.map((module, index) => ({
+        id: `module-${index}`,
+        name: module.name,
+        version: module.version ?? t('noVersionFound', 'No version found'),
+      })),
+    [installedModules],
+  );
+
   return (
-    <StructuredListWrapper ariaLabel="Structured list">
-      <StructuredListHead>
-        <StructuredListRow head tabIndex={0}>
-          <StructuredListCell head>Module name</StructuredListCell>
-          <StructuredListCell head>Version</StructuredListCell>
-        </StructuredListRow>
-      </StructuredListHead>
-      <StructuredListBody>
-        {installedModules.map((module) => (
-          <StructuredListRow tabIndex={0}>
-            <StructuredListCell>{module.name}</StructuredListCell>
-            <StructuredListCell>{module.version ?? 'No version found'} </StructuredListCell>
-          </StructuredListRow>
-        ))}
-      </StructuredListBody>
-    </StructuredListWrapper>
+    <DataTable
+      rows={rows}
+      headers={headers}
+      size="sm"
+      useZebraStyles
+      aria-label={t('frontendModules', 'Frontend modules')}>
+      {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+        <Table {...getTableProps()}>
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow {...getRowProps({ row })}>
+                {row.cells.map((cell) => (
+                  <TableCell key={cell.id}>{cell.value}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </DataTable>
   );
 };
 
